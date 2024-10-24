4fda2150b9b248b9eebcd1b1c365fdbb19e74081
darkmage <165251+mikedesu@users.noreply.github.com>
2024-10-24 18:06:17 -0500

    updated build stats and logging

--- src/log_build_stats.sh
+++ src/log_build_stats.sh
@@ -2,18 +2,32 @@ make clean;
 
 { time make } 2>time.txt;
 
-cat time.txt;
+mv game game_gcc;
 
 BUILD_TIME=$(awk '{print $8}' time.txt);
 
-echo $BUILD_TIME;
-
+#echo $BUILD_TIME;
+#echo $CLANG_BUILD_TIME;
 TIMESTAMP=$(date +%s);
 LOC=$(cat *.h *.c | egrep -v "^[[:space:]]*//" | egrep -v "^$" | wc -l);
 LUA_LOC=$(cat *.lua | egrep -v "^[[:space:]]*--" | egrep -v "^$" | wc -l);
-SIZE=$(ls -al game | awk '{print $5}');
-LIBSIZE=$(ls -al libgame.so | awk '{print $5}');
+GCC_SIZE=$(ls -al game_gcc | awk '{print $5}');
+GCC_LIBSIZE=$(ls -al libgame.so | awk '{print $5}');
+
+
+make clean;	
 #echo $TIMESTAMP,$SIZE,$LIBSIZE,$LOC,$BUILD_TIME;
-echo $TIMESTAMP,$SIZE,$LIBSIZE,$LOC,$LUA_LOC,$BUILD_TIME >> build-stats.csv
+#echo $TIMESTAMP,$GCC_SIZE,$CLANG_SIZE,$LIBSIZE,$LOC,$LUA_LOC,$BUILD_TIME >> build-stats.csv
+
+{ time make CC=clang } 2>time_clang.txt;
+mv game game_clang;
+CLANG_BUILD_TIME=$(awk '{print $9}' time_clang.txt);
+CLANG_SIZE=$(ls -al game_clang | awk '{print $5}');
+CLANG_LIBSIZE=$(ls -al libgame.so | awk '{print $5}');
 
 make clean;
+
+#echo "timestamp,gcc_size,clang_size,gcc_libsize,clang_libsize,loc,lua_loc,gcc_build_time,clang_build_time" > build-stats.csv
+echo $TIMESTAMP,$GCC_SIZE,$CLANG_SIZE,$GCC_LIBSIZE,$CLANG_LIBSIZE,$LOC,$LUA_LOC,$BUILD_TIME,$CLANG_BUILD_TIME >> build-stats.csv
+
+rm time.txt time_clang.txt game_gcc game_clang;
--- src/read_build_size.py
+++ src/read_build_size.py
@@ -20,8 +20,8 @@ def main():
 
     for index, row in df.iterrows():
         # df.at[index, "timestamp"] = pd.to_datetime(row["timestamp"], unit="s")
-        df.at[index, "binkb"] = row["binbytes"] // 1024
-        df.at[index, "libkb"] = row["libbytes"] // 1024
+        # df.at[index, "binkb"] = row["gcc_size"] // 1024
+        # df.at[index, "libkb"] = row["gcc_libsize"] // 1024
         # if we are at the first index, we cant calculate an LOC/s
         # however, every index past 0, we can subtract the timestamps to get how long between logs
         # then we can divide the loc by the time to get the loc/s
@@ -29,14 +29,17 @@ def main():
             pass
             time = row["timestamp"] - df.at[index - 1, "timestamp"]
             loc = row["loc"] - df.at[index - 1, "loc"]
+            lua_loc = row["lua_loc"] - df.at[index - 1, "lua_loc"]
             # bytes_ = row["binbytes"] - df.at[index - 1, "bytes"]
             df.at[index, "loc/s"] = loc / time
+            df.at[index, "lua_loc/s"] = lua_loc / time
             # df.at[index, "bytes/s"] = bytes_ / time
             # how fast the build time is increasing or decreasing per second
             # buildtime = row["buildtime"] - df.at[index - 1, "buildtime"]
             # df.at[index, "buildtime ms/s"] = buildtime / time
         else:
             df.at[index, "loc/s"] = 0
+            df.at[index, "lua_loc/s"] = 0
             # df.at[index, "bytes/s"] = 0
             # df.at[index, "buildtime ms/s"] = 0
 
@@ -48,9 +51,11 @@ def main():
 
     # buildtimestr = f"{df['buildtime'].mean():.4f}"
     locstr = f"{df['loc'].mean():.1f}"
-    kbstr = f"{df['binkb'].mean():.1f}"
-    kbstr2 = f"{df['libkb'].mean():.1f}"
-    buildtimestr = f"{df['buildtime'].mean():.3f}"
+    lua_locstr = f"{df['lua_loc'].mean():.1f}"
+    # kbstr = f"{df['binkb'].mean():.1f}"
+    # kbstr2 = f"{df['libkb'].mean():.1f}"
+    gccbuildtimestr = f"{df['gcc_build_time'].mean():.3f}"
+    clangbuildtimestr = f"{df['clang_build_time'].mean():.3f}"
 
     # bpsstr = f"{df['bytes/s'].mean():.2f}"
     # locsstr = f"{df['loc/s'].mean():.2f}"
@@ -62,8 +67,12 @@ def main():
     # print(f"Avg line count:   {locstr:>10} loc")
     # print(f"Avg binary size:  {kbstr:>10} kb")
     # print(f"Avg library size: {kbstr2:>10} kb")
-    print(f"Avg build Time:     {buildtimestr:>10} s")
-    print(f"Current build Time: {df.at[df.index[-1], 'buildtime']:>10} s")
+    print(f"GCC Avg build Time:       {gccbuildtimestr:>10} s")
+    print(f"GCC Current build Time:   {df.at[df.index[-1], 'gcc_build_time']:>10} s")
+
+    print(f"Clang Avg build Time:     {clangbuildtimestr:>10} s")
+    print(f"Clang Current build Time: {df.at[df.index[-1], 'clang_build_time']:>10} s")
+
     # print(f"Avg build size (bytes/s): {bpsstr}")
     # print(f"Avg line count (loc/s):   {locsstr}")
     # print(f"Avg build Time (ms/s):    {buildtimestr}")
