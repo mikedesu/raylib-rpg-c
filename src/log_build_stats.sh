9919714f00005be9fdc18eabb4e2f8f959481091
darkmage <165251+mikedesu@users.noreply.github.com>
2024-11-03 18:43:22 -0600

    stat logging

--- src/log_build_stats.sh
+++ src/log_build_stats.sh
@@ -25,16 +25,30 @@ CLANG_BUILD_TIME=$(awk '{print $9}' time_clang.txt);
 CLANG_SIZE=$(ls -al game_clang | awk '{print $5}');
 CLANG_LIBSIZE=$(ls -al libgame.so | awk '{print $5}');
 
+
+{ time make index.html } 2>time_emcc.txt;
+
+EMCC_BUILD_TIME=$(awk '{print $9}' time_emcc.txt);
+EMCC_DATA_SIZE=$(ls -al index.data | awk '{print $5}');
+EMCC_HTML_SIZE=$(ls -al index.html | awk '{print $5}');
+EMCC_JS_SIZE=$(ls -al index.js | awk '{print $5}');
+EMCC_WASM_SIZE=$(ls -al index.wasm | awk '{print $5}');
+
 make clean;
 
 #echo "timestamp,gcc_size,clang_size,gcc_libsize,clang_libsize,loc,lua_loc,gcc_build_time,clang_build_time" > build-stats.csv
-echo $TIMESTAMP,$GCC_SIZE,$CLANG_SIZE,$GCC_LIBSIZE,$CLANG_LIBSIZE,$LOC,$LUA_LOC,$BUILD_TIME,$CLANG_BUILD_TIME >> build-stats.csv
+echo $TIMESTAMP,$GCC_SIZE,$CLANG_SIZE,$GCC_LIBSIZE,$CLANG_LIBSIZE,$EMCC_DATA_SIZE,$EMCC_HTML_SIZE,$EMCC_JS_SIZE,$EMCC_WASM_SIZE,$LOC,$LUA_LOC,$BUILD_TIME,$CLANG_BUILD_TIME,$EMCC_BUILD_TIME >> build-stats.csv
 
-python3 graph_loc.py build-stats.csv loc 2>/dev/null;
-python3 graph_loc.py build-stats.csv lua_loc 2>/dev/null;
-python3 graph_loc.py build-stats.csv loc lua_loc 2>/dev/null;
-python3 graph_loc.py build-stats.csv gcc_build_time clang_build_time 2>/dev/null;
-python3 graph_loc.py build-stats.csv gcc_size clang_size 2>/dev/null;
-python3 graph_loc.py build-stats.csv gcc_libsize clang_libsize 2>/dev/null;
+#python3 graph_loc.py build-stats.csv loc 2>/dev/null;
+#python3 graph_loc.py build-stats.csv lua_loc 2>/dev/null;
+#python3 graph_loc.py build-stats.csv loc lua_loc 2>/dev/null;
+#python3 graph_loc.py build-stats.csv gcc_build_time clang_build_time 2>/dev/null;
+#python3 graph_loc.py build-stats.csv gcc_size clang_size 2>/dev/null;
+#python3 graph_loc.py build-stats.csv gcc_libsize clang_libsize 2>/dev/null;
+#python3 graph_loc.py build-stats.csv emcc_build_time 2>/dev/null;
+#python3 graph_loc.py build-stats.csv emcc_data_size 2>/dev/null;
+#python3 graph_loc.py build-stats.csv emcc_html_size 2>/dev/null;
+#python3 graph_loc.py build-stats.csv emcc_js_size 2>/dev/null;
+#python3 graph_loc.py build-stats.csv emcc_wasm_size 2>/dev/null;
 
-rm time.txt time_clang.txt game_gcc game_clang;
+rm -rf time.txt time_clang.txt time_emcc.txt;
