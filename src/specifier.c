1258672e1c6bea32fdf8e99038ecec127af268cb
darkmage <165251+mikedesu@users.noreply.github.com>
2024-11-06 18:40:55 -0600

    Revert "cleanup"
    This reverts commit cfb6ca0382eb99a374dbd90d7e2180caae3df070.

--- src/specifier.c
+++ src/specifier.c
@@ -1,12 +1,17 @@
 #include "specifier.h"
 
-const char* specifier_get_str(const specifier_t spec) {
+const char* specifier_get_str(specifier_t spec) {
     switch (spec) {
-    case SPECIFIER_NONE: return "SPECIFIER_NONE";
-    case SPECIFIER_SHIELD_ON_TILE: return "SPECIFIER_SHIELD_ON_TILE";
-    case SPECIFIER_SHIELD_BLOCK: return "SPECIFIER_SHIELD_BLOCK";
-    case SPECIFIER_SHIELD_BLOCK_SUCCESS: return "SPECIFIER_SHIELD_BLOCK_SUCCESS";
-    case SPECIFIER_COUNT: return "SPECIFIER_COUNT";
+    case SPECIFIER_NONE:
+        return "SPECIFIER_NONE";
+    case SPECIFIER_SHIELD_ON_TILE:
+        return "SPECIFIER_SHIELD_ON_TILE";
+    case SPECIFIER_SHIELD_BLOCK:
+        return "SPECIFIER_SHIELD_BLOCK";
+    case SPECIFIER_SHIELD_BLOCK_SUCCESS:
+        return "SPECIFIER_SHIELD_BLOCK_SUCCESS";
+    case SPECIFIER_COUNT:
+        return "SPECIFIER_COUNT";
     }
     return "SPECIFIER_UNKNOWN";
 }
