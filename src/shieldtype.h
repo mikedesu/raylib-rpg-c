bfdc94b7452d740586f981ba2b060770adcba4c8
darkmage <165251+mikedesu@users.noreply.github.com>
2025-03-26 14:58:29 -0500

    removed old files

--- src/shieldtype.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-typedef enum {
-    SHIELD_NONE,
-    SHIELD_BASIC,
-    SHIELD_COUNT
-
-} shieldtype_t;
--- src/weapontype.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-typedef enum {
-    WEAPON_NONE,
-    WEAPON_SWORD,
-    WEAPON_COUNT
-
-} weapontype_t;
