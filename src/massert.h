39259bee3c463b436fadf8ea1900abb2a7fd930c
darkmage <165251+mikedesu@users.noreply.github.com>
2025-03-29 12:38:49 -0500

    better massert

--- src/massert.h
+++ src/massert.h
@@ -1,12 +1,27 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
 
+// Cross-platform debug break
+#if defined(_WIN32)
+#include <windows.h>
+#define DEBUG_BREAK() DebugBreak()
+#elif defined(__linux__) || defined(__APPLE__)
+#include <signal.h>
+#define DEBUG_BREAK() raise(SIGTRAP)
+#else
+#define DEBUG_BREAK() ((void)0)
+#endif
+
+// Assert macro with printf formatting
 #ifdef DEBUG
 #define massert(cond, ...)                                                                                             \
     do {                                                                                                               \
         if (!(cond)) {                                                                                                 \
-            printf("☠️  \033[31;1mASSERT \033[0m %s:%d: ", __FILE__, __LINE__);                                         \
+            printf("☠️  \033[31;1mASSERT\033[0m %s:%d: ", __FILE__, __LINE__);                                          \
             printf(__VA_ARGS__);                                                                                       \
             printf("\n");                                                                                              \
+            DEBUG_BREAK();                                                                                             \
             exit(EXIT_FAILURE);                                                                                        \
         }                                                                                                              \
     } while (0)
