512800aa358f814daaa86395c8574b42a8d9fd0b
darkmage <165251+mikedesu@users.noreply.github.com>
2025-03-29 10:57:03 -0500

    better massert

--- src/massert.h
+++ src/massert.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #ifdef DEBUG
-#define massert(cond, msg)                                                                                             \
+#define massert(cond, ...)                                                                                             \
     do {                                                                                                               \
         if (!(cond)) {                                                                                                 \
-            printf("☠️  \033[31;1mASSERT \033[0m %s:%d: %s\n", __FILE__, __LINE__, msg);                                \
+            fprintf(stderr, "☠️  \033[31;1mASSERT \033[0m %s:%d: ", __FILE__, __LINE__);                                \
+            fprintf(stderr, __VA_ARGS__);                                                                              \
+            fprintf(stderr, "\n");                                                                                     \
             exit(EXIT_FAILURE);                                                                                        \
         }                                                                                                              \
     } while (0)
 #else
-#define massert(cond, msg) ((void)0)
+#define massert(cond, ...) ((void)0)
 #endif
