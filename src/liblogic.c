444d25a0dcf3cfb3b372f5fa6d57d92b347410ed
darkmage <165251+mikedesu@users.noreply.github.com>
2025-04-17 00:34:27 -0500

    cleanup

--- src/liblogic.c
+++ src/liblogic.c
@@ -1430,7 +1430,7 @@ void liblogic_init(gamestate* const g) {
     liblogic_init_em(g);
     liblogic_init_player(g);
     // test to create a weapon
-    //liblogic_init_weapon_test(g);
+    liblogic_init_weapon_test(g);
     // temporarily disabling
     //liblogic_init_orcs_test(g);
     liblogic_update_debug_panel_buffer(g);
