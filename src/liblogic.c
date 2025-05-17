7a44fd210e56afdf7c22f050c75382d488658e32
darkmage <165251+mikedesu@users.noreply.github.com>
2025-05-17 16:20:33 -0500

    added other weapon tests back in

--- src/liblogic.c
+++ src/liblogic.c
@@ -2091,9 +2091,9 @@ void liblogic_init(gamestate* const g) {
     //init_potion_test(g, POTION_HP_LARGE, "large healing potion");
     //init_npcs_test_by_room(g);
     //init_npc_test(g);
-    //init_sword_test(g);
+    init_sword_test(g);
     init_dagger_test(g);
-    //init_axe_test(g);
+    init_axe_test(g);
     //init_bow_test(g);
     init_shield_test(g);
     //init_potion_test(g);
