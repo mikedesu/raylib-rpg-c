dafbf42666249cefb7cd37981f999d0ab1075fc8
darkmage <165251+mikedesu@users.noreply.github.com>
2025-05-22 21:30:39 -0500

    refactor: Simplify test_dungeon_floors function and remove duplicate include

--- src/test_dungeon_floor.h
+++ src/test_dungeon_floor.h
@@ -2,22 +2,18 @@
 
 #include "dungeon_floor.h"
 
-#include "dungeon_floor.h"
-
 static void run_test_df_create_destroy(void) {
     dungeon_floor_t* df = df_create(DEFAULT_DUNGEON_FLOOR_WIDTH, DEFAULT_DUNGEON_FLOOR_HEIGHT);
     massert(df != NULL, "Failed to create dungeon floor");
-    
+
     // Basic sanity checks
     massert(df->width == DEFAULT_DUNGEON_FLOOR_WIDTH, "Incorrect width");
     massert(df->height == DEFAULT_DUNGEON_FLOOR_HEIGHT, "Incorrect height");
     massert(df->tiles != NULL, "Tiles array not allocated");
     massert(df->rooms != NULL, "Rooms array not allocated");
-    
+
     df_destroy(df);
     msuccess("df_create and df_destroy passed");
 }
 
-void test_dungeon_floors(void) {
-    run_test_df_create_destroy();
-}
+void test_dungeon_floors(void) { run_test_df_create_destroy(); }
