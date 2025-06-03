809b42e3c9dfe73f3c914fd57d3095d743d58254
darkmage <165251+mikedesu@users.noreply.github.com>
2025-06-02 22:49:22 -0500

    btree_int unit test beginning

--- src/unit_test.c
+++ src/unit_test.c
@@ -1,3 +1,4 @@
+#include "test_btree_int.h"
 #include "test_component_table.h"
 #include "test_dungeon.h"
 #include "test_dungeon_floor.h"
@@ -8,8 +9,9 @@ int main(void) {
     test_component_table();
     test_gamestate();
     test_dungeon_tiles();
-    test_dungeon_floors();
-    test_dungeon();
+    //test_dungeon_floors();
+    //test_dungeon();
+    test_btree_int_tests();
     unit_test_summary();
     return 0;
 }
