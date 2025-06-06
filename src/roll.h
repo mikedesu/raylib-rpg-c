3df3cfcae7007a9370ea184bcb08d72ea6aa2de2
darkmage <165251+mikedesu@users.noreply.github.com>
2025-06-06 17:14:17 -0500

    cleanup

--- src/roll.h
+++ src/roll.h
@@ -4,21 +4,12 @@
 #include "vec3.h"
 #include <stdlib.h>
 
-//typedef struct roll_t {
-//    int n;
-//    int sides;
-//    int modifier;
-//} roll;
-
-//static inline int do_roll(roll r) {
 static inline int do_roll(vec3 r) {
     massert(r.x > 0, "n is negative");
     massert(r.y > 0, "sides is negative");
     int total = 0;
     // if sides is 1, then we just return n + modifier
-    if (r.y == 1) {
-        return r.x + r.z;
-    }
+    if (r.y == 1) return r.x + r.z;
     for (int i = 0; i < r.x; i++) total += (rand() % r.y) + 1;
     return total + r.z;
 }
@@ -26,10 +17,6 @@ static inline int do_roll(vec3 r) {
 static inline int do_roll_best_of_3(vec3 r) {
     massert(r.x > 0, "n is negative");
     massert(r.y > 0, "sides is negative");
-    int a, b, c;
-    a = do_roll(r);
-    b = do_roll(r);
-    c = do_roll(r);
-    int best = a > b ? (a > c ? a : c) : (b > c ? b : c);
-    return best;
+    int a = do_roll(r), b = do_roll(r), c = do_roll(r);
+    return a > b ? (a > c ? a : c) : (b > c ? b : c);
 }
