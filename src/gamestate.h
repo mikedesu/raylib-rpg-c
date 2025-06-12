1aa1e450cde032fa600072348eaf4dc5c42890d6
darkmage <165251+mikedesu@users.noreply.github.com>
2025-06-12 02:56:22 -0500

    update

--- src/gamestate.h
+++ src/gamestate.h
@@ -22,7 +22,7 @@
 #include <stdbool.h>
 #include <time.h>
 
-#define GAME_VERSION "v0.0.3.12"
+#define GAME_VERSION "v0.0.3.13"
 
 #define GAMESTATE_SIZEOFTIMEBUF 64
 #define GAMESTATE_SIZEOFDEBUGPANELBUF 1024
