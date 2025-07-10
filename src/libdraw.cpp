9d9d2c833fc4124f1955177aa872b6814b6e2a0c
darkmage <165251+mikedesu@users.noreply.github.com>
2025-07-09 20:31:33 -0500

    cleanup

--- src/libdraw.cpp
+++ src/libdraw.cpp
@@ -1026,24 +1026,60 @@ static void draw_message_box(shared_ptr<gamestate> g) {
 
 
 static void draw_message_history(shared_ptr<gamestate> g) {
-    //if (!g->msg_system_is_active || g->msg_system->size() == 0) {
-    //    return;
-    //}
-    int font_size = 10;
-    Color message_bg = (Color){0x33, 0x33, 0x33, 0xff};
+    int font_size, msg_count, measure, pad;
+    Color message_bg;
+    //float x, y, w, h, text_width, text_height, msg_x, msg_y;
+    float x, y, w, h, text_width, msg_x, msg_y;
+    Rectangle box;
+    char tmp[1024] = {0};
+    string msg;
     // message history will now be a box directly beneath the hud
     // it should be static in size, that is, unchanging and const
     // first, lets calc that box position and size
-    float x, y, w, h;
-    w = 100;
-    h = font_size * 5 + g->pad * 2;
+
+    font_size = 10;
+    message_bg = {0x33, 0x33, 0x33, 0xff};
+
+    msg_count = g->msg_history->size();
+
+    if (msg_count == 0) {
+        // if there are no messages, we don't need to draw anything
+        return;
+    }
+
+    w = 320;
+
+    //h = font_size * 5 + g->pad * 2;
+    pad = 10;
+    h = font_size * msg_count + pad * 2;
+
     x = DEFAULT_TARGET_WIDTH - w;
     y = font_size * 2 + g->pad * 2;
 
-    Rectangle box = {x, y, w, h};
+    box = {x, y, w, h};
+
+
     DrawRectangleRec(box, message_bg);
     DrawRectangleLinesEx(box, 1, WHITE);
-    //DrawText(tmp, box.x + g->pad, box.y + g->pad, font_size, WHITE);
+
+    // now lets draw each message in the history
+    for (int i = 0; i < msg_count; i++) {
+        msg = g->msg_history->at(i);
+
+        bzero(tmp, 1024);
+        snprintf(tmp, sizeof(tmp), "%s", msg.c_str());
+
+        //measure = MeasureText(tmp, font_size);
+        text_width = MeasureText(tmp, font_size);
+        //text_height = font_size;
+        //text_width = measure;
+        msg_x = box.x + pad;
+        msg_y = box.y + pad + (i * (font_size + 2));
+        if (msg_y + font_size > box.y + box.height) {
+            break; // stop drawing if we exceed the box height
+        }
+        DrawText(tmp, msg_x, msg_y, font_size, WHITE);
+    }
 }
 
 
