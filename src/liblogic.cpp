5080f110d9221263231665389605fcc9e41d40a3
darkmage <165251+mikedesu@users.noreply.github.com>
2025-07-14 16:30:20 -0500

    try_entity_pickup has some bugs, need to update locations on tile add

--- src/liblogic.cpp
+++ src/liblogic.cpp
@@ -121,6 +121,7 @@ static void try_spawn_npc(shared_ptr<gamestate> const g);
 static const char* get_action_key(shared_ptr<gamestate> g, shared_ptr<inputstate> is);
 
 static bool try_entity_move(shared_ptr<gamestate> g, entityid id, vec3 loc);
+static bool try_entity_pickup(shared_ptr<gamestate> g, entityid id);
 
 static entityid create_wooden_box(shared_ptr<gamestate> g, vec3 loc);
 static entityid create_npc_set_stats(shared_ptr<gamestate> g, vec3 loc, race_t race);
@@ -508,6 +509,11 @@ static void handle_input_gameplay_controlmode_player(shared_ptr<gamestate> g, sh
         } else if (inputstate_is_pressed(is, KEY_APOSTROPHE)) {
             g_set_attacking(g, g->hero_id, true);
             g_set_update(g, g->hero_id, true);
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        } else if (inputstate_is_pressed(is, KEY_SEMICOLON)) {
+            //add_message(g, "pickup item (unimplemented)");
+
+            try_entity_pickup(g, g->hero_id);
             g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
         }
         //return;
@@ -2231,56 +2237,81 @@ static void handle_input_inventory(const inputstate* const is,
 */
 
 
-/*
-static bool try_entity_pickup(gamestate* const g, entityid id) {
+static bool try_entity_pickup(shared_ptr<gamestate> g, entityid id) {
     massert(g, "Game state is NULL!");
     massert(id != ENTITYID_INVALID, "Entity is NULL!");
     g_set_update(g, id, true);
     // check if the player is on a tile with an item
-    vec3 loc = g_get_location(g, id);
-    vec3 loc_cast = {loc.x, loc.y, loc.z};
-    dungeon_floor_t* const df = d_get_floor(g->d, loc.z);
+    vec3 loc = g_get_loc(g, id);
+    shared_ptr<dungeon_floor_t> df = d_get_floor(g->dungeon, loc.z);
     if (!df) {
         merror("Failed to get dungeon floor");
         return false;
     }
-    //tile_t* const tile = df_tile_at(df, loc_cast);
-    shared_ptr<tile_t> tile = df_tile_at(df, loc_cast);
+    shared_ptr<tile_t> tile = df_tile_at(df, loc);
     if (!tile) {
         merror("Failed to get tile");
         return false;
     }
     if (tile->entities->size() == 0) {
-        merror("No items on tile");
+        add_message(g, "No items on tile");
         return false;
     }
-    for (int i = 0; (size_t)i < tile->entities->size(); i++) {
+    for (size_t i = 0; i < tile->entities->size(); i++) {
         entityid itemid = tile->entities->at(i);
         entitytype_t type = g_get_type(g, itemid);
-        //minfo("Item %s type: %d", g_get_name(g, itemid), type);
+        //    //minfo("Item %s type: %d", g_get_name(g, itemid), type);
         if (type == ENTITY_ITEM) {
-            add_message_history(g,
-                                "%s picked up a %s",
-                                g_get_name(g, id).c_str(),
-                                g_get_name(g, itemid).c_str());
-            bool result = g_add_to_inventory(g, id, itemid);
-            if (!result) {
-                merror("Failed to add item to inventory");
-                return false;
-            } else {
-                //minfo("Item %s added to inventory", g_get_name(g, itemid));
-                tile_remove(tile, itemid);
+            // now, we need to check what type of item it is
+            itemtype itype = g_get_item_type(g, itemid);
+            if (itype == ITEM_WEAPON) {
+                // if it is a weapon, we will "pick it up" and equip it
+                // if we have no weapon equipped, the weapon will
+                // simply come off the tile and be attached to the
+                // equipped weapon slot
+                //
+                // if we have a weapon equipped, for right now, we will
+                // swap the equipped weapon with the weapon on the tile,
+                // that is, the weapon on the tile will be removed,
+                // the equipped weapon will be detached and added to the tile,
+                // and we will equip the new weapon
+
+                entityid equipped_wpn_id = g_get_equipped_weapon(g, id);
+                if (equipped_wpn_id == ENTITYID_INVALID) {
+                    tile_remove(tile, itemid);
+                    g_set_equipped_weapon(g, id, itemid);
+                    add_message(g, "Equipped itemid %d", itemid);
+                } else {
+                    tile_remove(tile, itemid);
+                    g_set_equipped_weapon(g, id, itemid);
+                    add_message(g, "Equipped itemid %d", itemid);
+                    tile_add(tile, equipped_wpn_id);
+                    add_message(g, "Added equipped_wpn_id %d", equipped_wpn_id);
+                }
+                if (g_get_type(g, id) == ENTITY_PLAYER) g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+                break;
             }
-            if (g_is_type(g, id, ENTITY_PLAYER)) {
-                g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-            }
-            return true;
         }
+        //        add_message_history(g,
+        //                            "%s picked up a %s",
+        //                            g_get_name(g, id).c_str(),
+        //                            g_get_name(g, itemid).c_str());
+        //        bool result = g_add_to_inventory(g, id, itemid);
+        //        if (!result) {
+        //            merror("Failed to add item to inventory");
+        //            return false;
+        //        } else {
+        //            //minfo("Item %s added to inventory", g_get_name(g, itemid));
+        //            tile_remove(tile, itemid);
+        //        }
+        //        if (g_is_type(g, id, ENTITY_PLAYER)) {
+        //            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        //        }
+        //        return true;
     }
-    add_message(g, "No items to pick up");
+    //add_message(g, "No items to pick up");
     return false;
 }
-*/
 
 //static inline void try_flip_switch(gamestate* const g, entity* const e, int x, int y, int fl) {
 //    massert(g, "Game state is NULL!");
