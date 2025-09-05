11e2f37c491e5deb569501191b02f5a736672949
darkmage <165251+mikedesu@users.noreply.github.com>
2025-09-05 13:20:18 -0500

    update

--- src/gamestate_inventory.cpp
+++ src/gamestate_inventory.cpp
@@ -58,3 +58,57 @@ shared_ptr<set<entityid>> g_get_inventory(shared_ptr<gamestate> g, entityid id) 
     merror("g_get_inventory: g->inventory_list is NULL");
     return nullptr; // Return nullptr if the id is not found
 }
+
+
+bool g_exists_in_inventory(shared_ptr<gamestate> g, entityid id, entityid item_id) {
+    massert(g, "g is NULL");
+    massert(id != ENTITYID_INVALID, "id is invalid");
+    massert(item_id != ENTITYID_INVALID, "item_id is invalid");
+    shared_ptr<set<entityid>> inventory = g_get_inventory(g, id);
+    if (!inventory) {
+        merror("could not find inventory for id %d", id);
+        return false;
+    }
+    return inventory->find(item_id) != inventory->end();
+}
+
+
+bool g_add_to_inventory(shared_ptr<gamestate> g, entityid id, entityid item_id) {
+    massert(g, "g is NULL");
+    massert(id != ENTITYID_INVALID, "id is invalid");
+    massert(item_id != ENTITYID_INVALID, "item_id is invalid");
+
+    if (g_exists_in_inventory(g, id, item_id)) {
+        merror("item is already in inventory");
+        return false;
+    }
+
+    shared_ptr<set<entityid>> inventory = g_get_inventory(g, id);
+    if (!inventory) {
+        merror("could not find inventory for id %d", id);
+        return false;
+    }
+
+    inventory->insert(item_id);
+    return true;
+}
+
+bool g_remove_from_inventory(shared_ptr<gamestate> g, entityid id, entityid item_id) {
+    massert(g, "g is NULL");
+    massert(id != ENTITYID_INVALID, "id is invalid");
+    massert(item_id != ENTITYID_INVALID, "item_id is invalid");
+
+    if (g_exists_in_inventory(g, id, item_id)) {
+        // perform remove
+        shared_ptr<set<entityid>> inventory = g_get_inventory(g, id);
+        if (!inventory) {
+            merror("could not find inventory for id %d", id);
+            return false;
+        }
+        inventory->erase(item_id);
+        return true;
+    }
+
+    merror("item doesnt exist in the inventory");
+    return false;
+}
--- src/gamestate_inventory.h
+++ src/gamestate_inventory.h
@@ -5,3 +5,7 @@
 bool g_add_inventory(shared_ptr<gamestate> g, entityid id);
 bool g_has_inventory(shared_ptr<gamestate> g, entityid id);
 shared_ptr<set<entityid>> g_get_inventory(shared_ptr<gamestate> g, entityid id);
+
+bool g_exists_in_inventory(shared_ptr<gamestate> g, entityid id, entityid item_id);
+bool g_add_to_inventory(shared_ptr<gamestate> g, entityid id, entityid item_id);
+bool g_remove_from_inventory(shared_ptr<gamestate> g, entityid id, entityid item_id);
