23509dad5d86ea78aa9a1707cff0e6007c974fd6
darkmage <165251+mikedesu@users.noreply.github.com>
2025-09-05 13:27:08 -0500

    update

--- src/liblogic_try_entity_pickup.cpp
+++ src/liblogic_try_entity_pickup.cpp
@@ -1,5 +1,6 @@
 #include "gamestate.h"
-#include "gamestate_equipped_weapon.h"
+//#include "gamestate_equipped_weapon.h"
+#include "gamestate_inventory.h"
 #include "liblogic_add_message.h"
 #include "massert.h"
 
@@ -29,57 +30,51 @@ bool try_entity_pickup(shared_ptr<gamestate> g, entityid id) {
         entitytype_t type = g_get_type(g, itemid);
         //    //minfo("Item %s type: %d", g_get_name(g, itemid), type);
         if (type == ENTITY_ITEM) {
-            // now, we need to check what type of item it is
-            itemtype itype = g_get_item_type(g, itemid);
-            if (itype == ITEM_WEAPON) {
-                // if it is a weapon, we will "pick it up" and equip it
-                // if we have no weapon equipped, the weapon will
-                // simply come off the tile and be attached to the
-                // equipped weapon slot
-                //
-                // if we have a weapon equipped, for right now, we will
-                // swap the equipped weapon with the weapon on the tile,
-                // that is, the weapon on the tile will be removed,
-                // the equipped weapon will be detached and added to the tile,
-                // and we will equip the new weapon
-
-                entityid equipped_wpn_id = g_get_equipped_weapon(g, id);
-                if (equipped_wpn_id == ENTITYID_INVALID) {
-                    tile_remove(tile, itemid);
-                    g_set_equipped_weapon(g, id, itemid);
-                    add_message(g, "Equipped itemid %d", itemid);
-                } else {
-                    tile_remove(tile, itemid);
-                    g_update_loc(g, equipped_wpn_id, (vec3){-1, -1, -1});
-                    g_set_equipped_weapon(g, id, itemid);
-                    add_message(g, "Equipped itemid %d", itemid);
-
-                    // update the loc of the equipped_wpn_id
-                    g_update_loc(g, equipped_wpn_id, loc);
-                    tile_add(tile, equipped_wpn_id);
-                    add_message(g, "Added equipped_wpn_id %d", equipped_wpn_id);
-                }
-                if (g_get_type(g, id) == ENTITY_PLAYER) g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                break;
+            if (g_add_to_inventory(g, id, itemid)) {
+                tile_remove(tile, itemid);
+                msuccess("item added to inventory");
+            } else {
+                merror("failed to add item to inventory");
             }
+
+            //    entityid equipped_wpn_id = g_get_equipped_weapon(g, id);
+            //    if (equipped_wpn_id == ENTITYID_INVALID) {
+            //        tile_remove(tile, itemid);
+            //        g_set_equipped_weapon(g, id, itemid);
+            //        add_message(g, "Equipped itemid %d", itemid);
+            //    } else {
+            //        tile_remove(tile, itemid);
+            //        g_update_loc(g, equipped_wpn_id, (vec3){-1, -1, -1});
+            //        g_set_equipped_weapon(g, id, itemid);
+            //        add_message(g, "Equipped itemid %d", itemid);
+            //
+            //                    // update the loc of the equipped_wpn_id
+            //                    g_update_loc(g, equipped_wpn_id, loc);
+            //                    tile_add(tile, equipped_wpn_id);
+            //                    add_message(g, "Added equipped_wpn_id %d", equipped_wpn_id);
+            //                }
+            if (g_get_type(g, id) == ENTITY_PLAYER) {
+                g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+            }
+            break;
         }
-        //        add_message_history(g,
-        //                            "%s picked up a %s",
-        //                            g_get_name(g, id).c_str(),
-        //                            g_get_name(g, itemid).c_str());
-        //        bool result = g_add_to_inventory(g, id, itemid);
-        //        if (!result) {
-        //            merror("Failed to add item to inventory");
-        //            return false;
-        //        } else {
-        //            //minfo("Item %s added to inventory", g_get_name(g, itemid));
-        //            tile_remove(tile, itemid);
-        //        }
-        //        if (g_is_type(g, id, ENTITY_PLAYER)) {
-        //            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-        //        }
-        //        return true;
     }
+    //        add_message_history(g,
+    //                            "%s picked up a %s",
+    //                            g_get_name(g, id).c_str(),
+    //                            g_get_name(g, itemid).c_str());
+    //        bool result = g_add_to_inventory(g, id, itemid);
+    //        if (!result) {
+    //            merror("Failed to add item to inventory");
+    //            return false;
+    //        } else {
+    //            //minfo("Item %s added to inventory", g_get_name(g, itemid));
+    //            tile_remove(tile, itemid);
+    //        }
+    //        if (g_is_type(g, id, ENTITY_PLAYER)) {
+    //            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+    //        }
+    //        return true;
     //add_message(g, "No items to pick up");
     return false;
 }
