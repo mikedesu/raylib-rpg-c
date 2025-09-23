918ec60beb2db345fc93c5df0720c136b56becfb
darkmage <165251+mikedesu@users.noreply.github.com>
2025-09-23 10:49:32 -0500

    cleanup

--- src/weapon.h
+++ src/weapon.h
@@ -1,9 +1,5 @@
 #pragma once
 
-
-#include <string>
-
-
 typedef enum
 {
     WEAPON_NONE,
@@ -16,17 +12,3 @@ typedef enum
     WEAPON_FLAIL,
     WEAPON_TYPE_COUNT
 } weapontype_t;
-
-
-// we need some weapontype-to-string functions
-//static inline std::string weapontype2str(weapontype type) {
-//    if (type == WEAPON_NONE) return "none";
-//    if (type == WEAPON_DAGGER) return "dagger";
-//    if (type == WEAPON_SWORD) return "sword";
-//    if (type == WEAPON_AXE) return "axe";
-//    if (type == WEAPON_BOW) return "bow";
-//    if (type == WEAPON_TWO_HANDED_SWORD) return "two-handed sword";
-//    if (type == WEAPON_WARHAMMER) return "warhammer";
-//    if (type == WEAPON_FLAIL) return "flail";
-//    return "unknown";
-//}
