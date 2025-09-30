4a18a52aea04b4cd4491cdec95743f5ea50197b2
darkmage <165251+mikedesu@users.noreply.github.com>
2025-09-30 15:53:03 -0500

    cleanup

--- src/libdraw_version.cpp
+++ src/libdraw_version.cpp
@@ -1,16 +1,10 @@
 #include "libdraw_version.h"
-//#include "libgame_defines.h"
 
 void draw_version(const shared_ptr<gamestate> g) {
     massert(g, "gamestate is NULL");
     const char* version = g->version.c_str();
-    int font_size = 10;
     char buffer[1024] = {0};
-    // also grab the current music track path
+    int x = 20, y = 20, font_size = 10;
     snprintf(buffer, sizeof(buffer), "%s", version);
-    //Vector2 text_size = MeasureTextEx(GetFontDefault(), buffer, font_size, 1.0f);
-    //int w = DEFAULT_TARGET_WIDTH;
-    int x = 20;
-    int y = 20;
     DrawText(buffer, x, y, font_size, WHITE);
 }
