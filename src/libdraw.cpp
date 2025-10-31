1f2c2561b9b5e76a8b4c70741280a211e5746891
darkmage <165251+mikedesu@users.noreply.github.com>
2025-10-30 20:03:17 -0500

    repositioned message history and player hud

--- src/libdraw.cpp
+++ src/libdraw.cpp
@@ -1001,19 +1001,24 @@ void draw_message_box(shared_ptr<gamestate> g) {
     if (!g->msg_system_is_active || g->msg_system->size() == 0) {
         return;
     }
-    string msg = g->msg_system->at(0);
-    Color message_bg = (Color){0x33, 0x33, 0x33, 0xff};
+
+    const string msg = g->msg_system->at(0);
+
+    const Color message_bg = (Color){0x33, 0x33, 0x33, 0xff};
+    const int font_size = 10;
+
+
     char tmp[1024] = {0};
     snprintf(tmp, sizeof(tmp), "%s", msg.c_str());
-    int font_size = 10;
-    int measure = MeasureText(tmp, font_size);
-    float text_width = measure;
-    float text_height = font_size;
-    int w = DEFAULT_TARGET_WIDTH;
-    // instead, lets center the msg box in the middle of the screen
-    float x = (w - text_width) / 2.0 - g->pad;
-    float y = (DEFAULT_TARGET_HEIGHT - text_height) / 2.0 - g->pad;
-    Rectangle box = {x, y, text_width + g->pad * 2, text_height + g->pad * 2};
+    const int measure = MeasureText(tmp, font_size);
+    const float text_width = measure;
+    const float text_height = font_size;
+    const int w = DEFAULT_TARGET_WIDTH;
+    const float x = (w - text_width) / 2.0 - g->pad;
+    const float y = (DEFAULT_TARGET_HEIGHT - text_height) / 2.0 - g->pad;
+
+    const Rectangle box = {x, y, text_width + g->pad * 2, text_height + g->pad * 2};
+
     DrawRectangleRec(box, message_bg);
     DrawRectangleLinesEx(box, 1, WHITE);
     DrawText(tmp, box.x + g->pad, box.y + g->pad, font_size, WHITE);
--- src/libdraw_message_history.cpp
+++ src/libdraw_message_history.cpp
@@ -1,53 +1,30 @@
 #include "libdraw_message_history.h"
 
 void draw_message_history(shared_ptr<gamestate> g) {
-    int font_size, msg_count, max_measure, pad;
-    Color message_bg;
-    //float x, y, w, h, text_width, text_height, msg_x, msg_y;
-    float x, y, w, h, msg_x, msg_y;
-    Rectangle box;
     char tmp[1024] = {0};
-    string msg;
-    font_size = 10;
-    // message history will now be a box directly beneath the hud
-    // it should be static in size, that is, unchanging and const
-    // first, lets calc that box position and size
-    message_bg = {0x33, 0x33, 0x33, 0xff};
-    msg_count = g->msg_history->size();
+    const int font_size = 10;
+    const Color message_bg = {0x33, 0x33, 0x33, 0xff};
+    const int msg_count = g->msg_history->size();
     if (msg_count == 0) {
         // if there are no messages, we don't need to draw anything
         return;
     }
-    //max_measure = -1;
-    //for (int i = 0; i < msg_count; i++) {
-    //    msg = g->msg_history->at(i);
-    //    measure = MeasureText(msg.c_str(), font_size);
-    //    if (measure > max_measure) {
-    //        max_measure = measure;
-    //    }
-    //}
-    max_measure = g->msg_history_max_len_msg_measure;
-    w = max_measure + g->pad;
-    //h = font_size * 5 + g->pad * 2;
-    pad = 10;
-    h = font_size * msg_count + pad * 2;
+    const int max_measure = g->msg_history_max_len_msg_measure;
+    const float w = max_measure + g->pad;
+    const float h = font_size * msg_count + g->pad;
     //x = DEFAULT_TARGET_WIDTH - w;
-    x = 0;
-    y = font_size + g->pad + 5;
-    box = {x, y, w, h};
+    const float x = 0;
+    const float y = 0;
+    const Rectangle box = {x, y, w, h};
     DrawRectangleRec(box, message_bg);
     DrawRectangleLinesEx(box, 1, WHITE);
     // now lets draw each message in the history
     for (int i = 0; i < msg_count; i++) {
-        msg = g->msg_history->at(i);
+        const string msg = g->msg_history->at(i);
         bzero(tmp, 1024);
         snprintf(tmp, sizeof(tmp), "%s", msg.c_str());
-        //measure = MeasureText(tmp, font_size);
-        //text_width = MeasureText(tmp, font_size);
-        //text_height = font_size;
-        //text_width = measure;
-        msg_x = box.x + pad;
-        msg_y = box.y + pad + (i * (font_size + 2));
+        const float msg_x = box.x + g->pad / 2.0;
+        const float msg_y = box.y + g->pad / 2.0 + (i * (font_size + 2));
         if (msg_y + font_size > box.y + box.height) {
             break; // stop drawing if we exceed the box height
         }
