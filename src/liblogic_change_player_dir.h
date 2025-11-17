b30e5c168aff21d86952191e64ae7f16d9f330c9
darkmage <165251+mikedesu@users.noreply.github.com>
2025-11-16 23:12:10 -0600

    header-only version

--- src/liblogic_change_player_dir.h
+++ src/liblogic_change_player_dir.h
@@ -1,3 +1,14 @@
 #pragma once
+
 #include "gamestate.h"
-void change_player_dir(shared_ptr<gamestate> g, direction_t dir);
+
+void change_player_dir(shared_ptr<gamestate> g, direction_t dir) {
+    massert(g, "Game state is NULL!");
+    if (g->ct.get<dead>(g->hero_id).value_or(true)) {
+        return;
+    }
+    g->ct.set<direction>(g->hero_id, dir);
+    g->ct.set<update>(g->hero_id, true);
+    g->player_changing_dir = false;
+    g->frame_dirty = true;
+}
