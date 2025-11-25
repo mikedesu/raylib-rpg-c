f4f9dac5b00e17487825f0036a8d12419eedd033
darkmage <165251+mikedesu@users.noreply.github.com>
2025-11-25 13:24:59 -0600

    critical bugfix - random music index could be out of bounds. fixed

--- src/libdraw_load_music.cpp
+++ src/libdraw_load_music.cpp
@@ -4,7 +4,7 @@ void libdraw_load_music(shared_ptr<gamestate> g) {
     minfo("BEGIN load_music");
     // load the music stream from the selected path
     // randomly select a music path
-    size_t index = GetRandomValue(0, g->music_file_paths->size());
+    size_t index = GetRandomValue(0, g->music_file_paths->size() - 1);
     const char* music_path = g->music_file_paths->at(index).c_str();
     minfo("Music path: %s", music_path);
     music = LoadMusicStream(music_path);
