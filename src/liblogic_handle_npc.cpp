206e85adbb3a6ade4c9b10bf54bf8ff1287c5758
darkmage <165251+mikedesu@users.noreply.github.com>
2025-11-24 19:44:42 -0600

    enemies attack again

--- src/liblogic_handle_npc.cpp
+++ src/liblogic_handle_npc.cpp
@@ -21,17 +21,17 @@ void handle_npc(shared_ptr<gamestate> g, entityid id) {
             bool is_dead = g->ct.get<dead>(id).value();
 
             if (!is_dead) {
-                //entityid target_id = g->ct.get<target>(id).value_or(g->hero_id);
+                entityid target_id = g->ct.get<target>(id).value_or(g->hero_id);
                 //entityid target_id = g->hero_id;
 
-                //if (is_entity_adjacent(g, id, target_id)) {
-                // if id is adjacent to its target or the hero
-                //vec3 loc = g->ct.get<location>(target_id).value();
-                //try_entity_attack(g, id, loc.x, loc.y);
-                //} else {
-                // else, randomly move
-                try_entity_move(g, id, (vec3){rand() % 3 - 1, rand() % 3 - 1, 0});
-                //}
+                if (is_entity_adjacent(g, id, target_id)) {
+                    // if id is adjacent to its target or the hero
+                    vec3 loc = g->ct.get<location>(target_id).value();
+                    try_entity_attack(g, id, loc.x, loc.y);
+                } else {
+                    // else, randomly move
+                    try_entity_move(g, id, (vec3){rand() % 3 - 1, rand() % 3 - 1, 0});
+                }
             }
         }
         //execute_action(g, id, g_get_default_action(g, id));
