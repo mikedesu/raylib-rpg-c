33ff17c8d139be4cf8bb0407b39002da9522295b
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-01 11:58:24 -0600

    chore: Add placeholder for lowercase conversion in generate_sfx_header.sh

--- src/generate_sfx_header.sh
+++ src/generate_sfx_header.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/zsh
 
+
+# lowercase all files in audio/sfx/*.mp3
+# YOUR CODE HERE
+
+
+
+# generate the sfx.h
 FILENAME="sfx.h";
-
 echo "#pragma once" > $FILENAME;
-
 ct=0; 
 for i in `cat sfx.txt`; do 
     a=$(echo SFX_$i $ct | sed 's/.wav//' | tr '[:lower:]' '[:upper:]'); 
