db15d4198d8e03916b675336731e7da34b56add5
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-05 16:31:19 -0600

    refactor: split create_npc into separate functions

--- src/create_npc.cpp
+++ src/create_npc.cpp
@@ -5,52 +5,11 @@
 #include "race.h"
 #include <raylib.h>
 
-entityid create_npc(shared_ptr<gamestate> g, race_t rt, vec3 loc, const string n) {
-    minfo("begin create npc");
-    massert(g, "gamestate is NULL");
 
-    shared_ptr<dungeon_floor_t> df = d_get_floor(g->dungeon, loc.z);
-    shared_ptr<tile_t> tile = df_tile_at(df, loc);
-
-    massert(tile, "failed to get tile");
-
-    if (!tile_is_walkable(tile->type)) {
-        merror("cannot create entity on non-walkable tile");
-        return ENTITYID_INVALID;
-    }
-
-    if (tile_has_live_npcs(g, tile)) {
-        merror("cannot create entity on tile with live NPCs");
-        return ENTITYID_INVALID;
-    }
-
-    entityid id = g_add_entity(g);
-
-    g->ct.set<name>(id, n);
-    g->ct.set<entitytype>(id, ENTITY_NPC);
-    g->ct.set<race>(id, rt);
-    g->ct.set<location>(id, loc);
-    g->ct.set<spritemove>(id, (Rectangle){0, 0, 0, 0});
-    g->ct.set<dead>(id, false);
-    g->ct.set<update>(id, true);
-    g->ct.set<direction>(id, DIR_DOWN_RIGHT);
-    g->ct.set<attacking>(id, false);
-    g->ct.set<blocking>(id, false);
-    g->ct.set<block_success>(id, false);
-    g->ct.set<damaged>(id, false);
-    g->ct.set<txalpha>(id, 0);
-    g->ct.set<inventory>(id, make_shared<vector<entityid>>());
-    g->ct.set<equipped_weapon>(id, ENTITYID_INVALID);
-    g->ct.set<aggro>(id, false);
-
-    g->ct.set<vision_distance>(id, 3);
-    g->ct.set<hearing_distance>(id, 3);
-
-    // here we have some hard decisions to make about how to template-out NPC creation
-    // all NPCs begin at level 1. level-up mechanisms will be determined elsewhere
-    g->ct.set<level>(id, 1);
-    g->ct.set<xp>(id, 0);
-
+void set_npc_starting_stats(shared_ptr<gamestate> g, entityid id) {
+    auto rt = g->ct.get<race>(id).value_or(RACE_NONE);
+    if (rt == RACE_NONE)
+        return;
 
     // stats racial modifiers for stats
     int str_m = get_racial_modifiers(rt, 0);
@@ -97,31 +56,61 @@ entityid create_npc(shared_ptr<gamestate> g, race_t rt, vec3 loc, const string n
 
     g->ct.set<maxhp>(id, my_maxhp);
     g->ct.set<hp>(id, my_hp);
+}
 
+
+void set_npc_defaults(shared_ptr<gamestate> g, entityid id) {
+    g->ct.set<entitytype>(id, ENTITY_NPC);
+    g->ct.set<spritemove>(id, (Rectangle){0, 0, 0, 0});
+    g->ct.set<dead>(id, false);
+    g->ct.set<update>(id, true);
+    g->ct.set<direction>(id, DIR_DOWN_RIGHT);
+    g->ct.set<attacking>(id, false);
+    g->ct.set<blocking>(id, false);
+    g->ct.set<block_success>(id, false);
+    g->ct.set<damaged>(id, false);
+    g->ct.set<txalpha>(id, 0);
+    g->ct.set<inventory>(id, make_shared<vector<entityid>>());
+    g->ct.set<equipped_weapon>(id, ENTITYID_INVALID);
+    g->ct.set<aggro>(id, false);
+    g->ct.set<vision_distance>(id, 3);
+    g->ct.set<hearing_distance>(id, 3);
+    // here we have some hard decisions to make about how to template-out NPC creation
+    // all NPCs begin at level 1. level-up mechanisms will be determined elsewhere
+    g->ct.set<level>(id, 1);
+    g->ct.set<xp>(id, 0);
+}
+
+
+entityid create_npc(shared_ptr<gamestate> g, race_t rt, vec3 loc, const string n) {
+    minfo("begin create npc");
+    massert(g, "gamestate is NULL");
+
+    auto df = d_get_floor(g->dungeon, loc.z);
+    auto tile = df_tile_at(df, loc);
+
+    massert(tile, "failed to get tile");
+
+    if (!tile_is_walkable(tile->type)) {
+        merror("cannot create entity on non-walkable tile");
+        return ENTITYID_INVALID;
+    }
+
+    if (tile_has_live_npcs(g, tile)) {
+        merror("cannot create entity on tile with live NPCs");
+        return ENTITYID_INVALID;
+    }
+
+    entityid id = g_add_entity(g);
+    set_npc_defaults(g, id);
+
+    g->ct.set<name>(id, n);
+    g->ct.set<race>(id, rt);
+    g->ct.set<location>(id, loc);
+
+    set_npc_starting_stats(g, id);
 
     minfo("end create npc");
 
     return df_add_at(df, id, loc.x, loc.y);
-
-    //g_add_zapping(g, id, false);
-    //g_add_default_action(g, id, ENTITY_ACTION_WAIT);
-    //g_add_inventory(g, id);
-    //g_add_target(g, id, (vec3){-1, -1, -1});
-    //g_add_target_path(g, id);
-    //g_add_equipment(g, id);
-    //g_add_base_attack_damage(g, id, (vec3){1, 4, 0});
-    //g_add_vision_distance(g, id, 0);
-    //g_add_light_radius(g, id, 0);
-    //g_add_stats(g, id);
-    //g_set_stat(g, id, STATS_LEVEL, 1);
-    //g_set_stat(g, id, STATS_XP, 0);
-    //g_set_stat(g, id, STATS_NEXT_LEVEL_XP, calc_next_lvl_xp(g, id));
-    //g_set_stat(g, id, STATS_MAXHP, 1);
-    //g_set_stat(g, id, STATS_HP, 1);
-    //g_set_stat(g, id, STATS_HITDIE, 1);
-    //g_set_stat(g, id, STATS_STR, 10);
-    //g_set_stat(g, id, STATS_DEX, 10);
-    //g_set_stat(g, id, STATS_CON, 10);
-    //g_set_stat(g, id, STATS_ATTACK_BONUS, 0);
-    //g_set_stat(g, id, STATS_AC, 10);
 }
--- src/create_npc.h
+++ src/create_npc.h
@@ -3,3 +3,5 @@
 #include "gamestate.h"
 
 entityid create_npc(shared_ptr<gamestate> g, race_t rt, vec3 loc, const string n);
+void set_npc_defaults(shared_ptr<gamestate> g, entityid id);
+void set_npc_starting_stats(shared_ptr<gamestate> g, entityid id);
