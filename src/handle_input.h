f2dd4649a4706169c2bf9f4dd3a69b3ea9681394
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-10 16:44:07 -0600

    update

--- src/handle_input.h
+++ src/handle_input.h
@@ -13,28 +13,18 @@ static inline void handle_input(shared_ptr<gamestate> g, shared_ptr<inputstate> 
     massert(g, "gamestate is NULL");
 
     // no matter which mode we are in, we can toggle the debug panel
-
     if (inputstate_is_pressed(is, KEY_P)) {
         g->debugpanelon = !g->debugpanelon;
         minfo("Toggling debug panel: %s", g->debugpanelon ? "ON" : "OFF");
         return;
     }
 
-    //switch (g->current_scene) {
-    //case SCENE_TITLE:
-    //case SCENE_MAIN_MENU: handle_input_title_scene(g, is); break;
-    //case SCENE_CHARACTER_CREATION: handle_input_character_creation_scene(g, is); break;
-    //case SCENE_GAMEPLAY: handle_input_title_scene(g, is); break;
-    //default: break;
-    //}
-
-    if (g->current_scene == SCENE_TITLE) {
+    if (g->current_scene == SCENE_TITLE)
         handle_input_title_scene(g, is);
-    } else if (g->current_scene == SCENE_MAIN_MENU) {
+    else if (g->current_scene == SCENE_MAIN_MENU)
         handle_input_main_menu_scene(g, is);
-    } else if (g->current_scene == SCENE_CHARACTER_CREATION) {
+    else if (g->current_scene == SCENE_CHARACTER_CREATION)
         handle_input_character_creation_scene(g, is);
-    } else if (g->current_scene == SCENE_GAMEPLAY) {
+    else if (g->current_scene == SCENE_GAMEPLAY)
         handle_input_gameplay_scene(g, is);
-    }
 }
