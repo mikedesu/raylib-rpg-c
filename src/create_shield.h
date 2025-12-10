340e4a6d5b02eeba9122b357c36b3af70f1d492d
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-09 22:24:32 -0600

    update

--- src/create_shield.h
+++ src/create_shield.h
@@ -2,5 +2,21 @@
 
 #include "gamestate.h"
 
-entityid create_shield_with(shared_ptr<gamestate> g, function<void(shared_ptr<gamestate>, entityid)> shieldInitFunction);
-entityid create_shield_at_with(shared_ptr<gamestate> g, vec3 loc, function<void(shared_ptr<gamestate>, entityid)> shieldInitFunction);
+static inline entityid create_shield_with(shared_ptr<gamestate> g, function<void(shared_ptr<gamestate>, entityid)> shieldInitFunction) {
+    const auto id = g_add_entity(g);
+    g->ct.set<entitytype>(id, ENTITY_ITEM);
+    g->ct.set<itemtype>(id, ITEM_SHIELD);
+    shieldInitFunction(g, id);
+    return id;
+}
+
+
+static inline entityid create_shield_at_with(shared_ptr<gamestate> g, vec3 loc, function<void(shared_ptr<gamestate>, entityid)> shieldInitFunction) {
+    const auto id = create_shield_with(g, shieldInitFunction);
+    minfo("attempting df_add_at: %d, %d, %d", id, loc.x, loc.y);
+    auto df = d_get_floor(g->dungeon, loc.z);
+    if (!df_add_at(df, id, loc.x, loc.y))
+        return ENTITYID_INVALID;
+    g->ct.set<location>(id, loc);
+    return id;
+}
