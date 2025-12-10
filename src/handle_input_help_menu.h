08b5ce7999c4854b86f3afda88be4e2f7106f107
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-10 15:59:15 -0600

    update

--- src/handle_input_help_menu.h
+++ src/handle_input_help_menu.h
@@ -1,6 +1,9 @@
-#include "liblogic_handle_input_help_menu.h"
+#pragma once
 
-void handle_input_help_menu(shared_ptr<gamestate> g, shared_ptr<inputstate> is) {
+#include "gamestate.h"
+#include "inputstate.h"
+
+static inline void handle_input_help_menu(shared_ptr<gamestate> g, shared_ptr<inputstate> is) {
     massert(is, "Input state is NULL!");
     massert(g, "Game state is NULL!");
     if (inputstate_any_pressed(is)) {
