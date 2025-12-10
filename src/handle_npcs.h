0d878a36ff1b60eec96195b92c7a453c60b53f4a
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-10 15:55:23 -0600

    update

--- src/handle_npcs.h
+++ src/handle_npcs.h
@@ -1,7 +1,11 @@
-#include "handle_npc.h"
-#include "liblogic_handle_npcs.h"
 
-void handle_npcs(shared_ptr<gamestate> g) {
+
+#pragma once
+
+#include "gamestate.h"
+#include "handle_npc.h"
+
+static inline void handle_npcs(shared_ptr<gamestate> g) {
     if (g->flag == GAMESTATE_FLAG_NPC_TURN) {
         if (g->entity_turn >= 0 && g->entity_turn < g->next_entityid) {
             handle_npc(g, g->entity_turn);
--- src/liblogic_handle_npcs.h
+++
@@ -1,6 +0,0 @@
-
-
-#pragma once
-
-#include "gamestate.h"
-void handle_npcs(shared_ptr<gamestate> g);
