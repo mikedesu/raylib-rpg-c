ef06686cd3d2df62fd6b3a2f2f29dca2dff23791
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-11 03:28:27 -0600

    update

--- src/libdraw_load_sfx.h
+++
@@ -1,8 +0,0 @@
-
-
-
-#pragma once
-
-#include "gamestate.h"
-
-void libdraw_load_sfx(shared_ptr<gamestate> g);
--- src/load_sfx.h
+++ src/load_sfx.h
@@ -1,13 +1,14 @@
-#include "libdraw_load_sfx.h"
+#pragma once
 
-void libdraw_load_sfx(shared_ptr<gamestate> g) {
+#include "gamestate.h"
+
+static inline void libdraw_load_sfx(shared_ptr<gamestate> g) {
     FILE* infile = fopen("sfx.txt", "r");
     char buffer[128];
     while (fgets(buffer, sizeof(buffer), infile) != NULL) {
         size_t len = strlen(buffer);
-        if (len > 0 && buffer[len - 1] == '\n') {
+        if (len > 0 && buffer[len - 1] == '\n')
             buffer[len - 1] = '\0';
-        }
         string fullpath = "audio/sfx/" + string(buffer);
         Sound sound = LoadSound(fullpath.c_str());
         g->sfx->push_back(sound);
