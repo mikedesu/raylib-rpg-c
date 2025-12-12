d82b591d2be798152fb8455c63ebc33fcece3d62
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-11 19:10:00 -0600

    update

--- src/handle_input_gameplay_controlmode_player.h
+++ src/handle_input_gameplay_controlmode_player.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "add_message.h"
+#include "change_player_dir.h"
 #include "cycle_messages.h"
 #include "gamestate.h"
 #include "get_loc_facing_player.h"
 #include "handle_input_help_menu.h"
-#include "liblogic_change_player_dir.h"
 #include "sfx.h"
 #include "try_entity_attack.h"
 #include "try_entity_move.h"
@@ -22,6 +22,7 @@ static inline void handle_input_gameplay_controlmode_player(shared_ptr<gamestate
 
     if (g->display_help_menu) {
         handle_input_help_menu(g, is);
+        return;
     }
 
     if (g->msg_system_is_active) {
@@ -37,7 +38,6 @@ static inline void handle_input_gameplay_controlmode_player(shared_ptr<gamestate
         if (inputstate_is_pressed(is, KEY_LEFT_BRACKET)) {
             g->cam2d.zoom += DEFAULT_ZOOM_INCR;
             g->frame_dirty = true;
-            //return;
         } else if (inputstate_is_pressed(is, KEY_RIGHT_BRACKET)) {
             g->cam2d.zoom -= (g->cam2d.zoom > 1.0) ? DEFAULT_ZOOM_INCR : 0.0;
             g->frame_dirty = true;
@@ -65,14 +65,12 @@ static inline void handle_input_gameplay_controlmode_player(shared_ptr<gamestate
             } else if (inputstate_is_pressed(is, KEY_C)) {
                 change_player_dir(g, DIR_DOWN_RIGHT);
             } else if (inputstate_is_pressed(is, KEY_APOSTROPHE)) {
-                //            g_set_attacking(g, g->hero_id, true);
+                // this is incorrect
+                // this should be a regular attack
                 g->ct.set<attacking>(g->hero_id, true);
-                //            g_set_update(g, g->hero_id, true);
                 g->ct.set<update>(g->hero_id, true);
                 g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
                 g->player_changing_dir = false;
-
-                //PlaySound(g->sfx->at(SFX_SLASH_ATTACK_SWORD_1));
             }
             return;
         }
@@ -91,125 +89,69 @@ static inline void handle_input_gameplay_controlmode_player(shared_ptr<gamestate
             return;
         }
 
-        if (g->hero_id != ENTITYID_INVALID) {
-            // make sure player isnt dead
-            optional<bool> maybe_player_is_dead = g->ct.get<dead>(g->hero_id);
-            if (maybe_player_is_dead.has_value()) {
-                bool player_is_dead = maybe_player_is_dead.value();
-
-                // Handling movement
-                if (inputstate_is_pressed(is, KEY_UP) || inputstate_is_pressed(is, KEY_W)) {
-                    if (!player_is_dead) {
-                        try_entity_move(g, g->hero_id, (vec3){0, -1, 0});
-                    } else {
-                        add_message(g, "You cant move while dead!");
-                    }
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                } else if (inputstate_is_pressed(is, KEY_DOWN) || inputstate_is_pressed(is, KEY_X)) {
-                    if (!player_is_dead) {
-                        try_entity_move(g, g->hero_id, (vec3){0, 1, 0});
-                    } else {
-                        add_message(g, "You cant move while dead!");
-                    }
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                } else if (inputstate_is_pressed(is, KEY_LEFT) || inputstate_is_pressed(is, KEY_A)) {
-                    if (!player_is_dead) {
-                        try_entity_move(g, g->hero_id, (vec3){-1, 0, 0});
-                    } else {
-                        add_message(g, "You cant move while dead!");
-                    }
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                } else if (inputstate_is_pressed(is, KEY_RIGHT) || inputstate_is_pressed(is, KEY_D)) {
-                    if (!player_is_dead) {
-                        try_entity_move(g, g->hero_id, (vec3){1, 0, 0});
-                    } else {
-                        add_message(g, "You cant move while dead!");
-                    }
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                } else if (inputstate_is_pressed(is, KEY_Q)) {
-                    if (!player_is_dead) {
-                        try_entity_move(g, g->hero_id, (vec3){-1, -1, 0});
-                    } else {
-                        add_message(g, "You cant move while dead!");
-                    }
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                } else if (inputstate_is_pressed(is, KEY_E)) {
-                    if (!player_is_dead) {
-                        try_entity_move(g, g->hero_id, (vec3){1, -1, 0});
-                    } else {
-                        add_message(g, "You cant move while dead!");
-                    }
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                } else if (inputstate_is_pressed(is, KEY_Z)) {
-                    if (!player_is_dead) {
-                        try_entity_move(g, g->hero_id, (vec3){-1, 1, 0});
-                    } else {
-                        add_message(g, "You cant move while dead!");
-                    }
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                } else if (inputstate_is_pressed(is, KEY_C)) {
-                    if (!player_is_dead) {
-                        try_entity_move(g, g->hero_id, (vec3){1, 1, 0});
-                    } else {
-                        add_message(g, "You cant move while dead!");
-                    }
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                    // Handling attack
-                } else if (inputstate_is_pressed(is, KEY_APOSTROPHE)) {
-                    if (!player_is_dead) {
-                        if (g->ct.get<location>(g->hero_id).has_value() && g->ct.get<direction>(g->hero_id).has_value()) {
-                            //vec3 loc = g->ct.get<location>(g->hero_id).value();
-                            vec3 loc = get_loc_facing_player(g);
-
-                            try_entity_attack(g, g->hero_id, loc.x, loc.y);
-
-                            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                        }
-                    } else {
-                        add_message(g, "You cant attack while dead!");
-                    }
-                }
-
-                // handling pickup item
-                else if (inputstate_is_pressed(is, KEY_SLASH)) {
-                    //add_message(g, "pickup item (unimplemented)");
-                    if (!player_is_dead) {
-                        try_entity_pickup(g, g->hero_id);
-                    } else {
-                        add_message(g, "You cant pickup items while dead!");
-                    }
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                }
-
-                // handling go up/down stairs
-                else if (inputstate_is_pressed(is, KEY_PERIOD)) {
-                    // what happens when we go up or down stairs?
-                    try_entity_stairs(g, g->hero_id);
-                }
-
-                else if (inputstate_is_pressed(is, KEY_O)) {
-                    vec3 loc = get_loc_facing_player(g);
-
-                    try_entity_open_door(g, g->hero_id, loc);
-                    g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
-                }
-
-
-                //else {
-                // Player is dead
-                //add_message(g, "You cannot move while dead");
-                //}
-            }
-
-
-            //else if (inputstate_is_pressed(is, KEY_SLASH) && inputstate_is_shift_held(is)) {
-            //    // open inventory
-            //    //g->inventory_is_open = !g->inventory_is_open;
-            //    g->display_help_menu = !g->display_help_menu;
-            //    g->frame_dirty = true;
-            //}
-
+        if (g->hero_id == ENTITYID_INVALID)
             return;
+
+        // make sure player isnt dead
+        auto maybe_player_is_dead = g->ct.get<dead>(g->hero_id);
+        if (!maybe_player_is_dead.has_value())
+            return;
+
+        const bool player_is_dead = maybe_player_is_dead.value();
+        if (player_is_dead) {
+            add_message(g, "You cant act while dead!");
+            return;
+        }
+
+
+        // Handling movement
+        if (inputstate_is_pressed(is, KEY_UP) || inputstate_is_pressed(is, KEY_W)) {
+            try_entity_move(g, g->hero_id, (vec3){0, -1, 0});
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        } else if (inputstate_is_pressed(is, KEY_DOWN) || inputstate_is_pressed(is, KEY_X)) {
+            try_entity_move(g, g->hero_id, (vec3){0, 1, 0});
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        } else if (inputstate_is_pressed(is, KEY_LEFT) || inputstate_is_pressed(is, KEY_A)) {
+            try_entity_move(g, g->hero_id, (vec3){-1, 0, 0});
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        } else if (inputstate_is_pressed(is, KEY_RIGHT) || inputstate_is_pressed(is, KEY_D)) {
+            try_entity_move(g, g->hero_id, (vec3){1, 0, 0});
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        } else if (inputstate_is_pressed(is, KEY_Q)) {
+            try_entity_move(g, g->hero_id, (vec3){-1, -1, 0});
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        } else if (inputstate_is_pressed(is, KEY_E)) {
+            try_entity_move(g, g->hero_id, (vec3){1, -1, 0});
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        } else if (inputstate_is_pressed(is, KEY_Z)) {
+            try_entity_move(g, g->hero_id, (vec3){-1, 1, 0});
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        } else if (inputstate_is_pressed(is, KEY_C)) {
+            try_entity_move(g, g->hero_id, (vec3){1, 1, 0});
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        }
+        // Handling attack
+        else if (inputstate_is_pressed(is, KEY_APOSTROPHE)) {
+            if (g->ct.get<location>(g->hero_id).has_value() && g->ct.get<direction>(g->hero_id).has_value()) {
+                const vec3 loc = get_loc_facing_player(g);
+                try_entity_attack(g, g->hero_id, loc.x, loc.y);
+                g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+            }
+        }
+        // handling pickup item
+        else if (inputstate_is_pressed(is, KEY_SLASH)) {
+            try_entity_pickup(g, g->hero_id);
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
+        }
+        // handling go up/down stairs
+        // what happens when we go up or down stairs?
+        else if (inputstate_is_pressed(is, KEY_PERIOD))
+            try_entity_stairs(g, g->hero_id);
+        // handling open door
+        else if (inputstate_is_pressed(is, KEY_O)) {
+            const vec3 loc = get_loc_facing_player(g);
+            try_entity_open_door(g, g->hero_id, loc);
+            g->flag = GAMESTATE_FLAG_PLAYER_ANIM;
         }
     }
 }
