7d81f4e3241c03bde347877e7313a9d5f91f644a
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-14 01:53:49 -0600

    rename and header-only

--- src/libdraw_update_weapon_for_entity.h
+++ src/libdraw_update_weapon_for_entity.h
@@ -2,5 +2,35 @@
 
 #include "gamestate.h"
 #include "spritegroup.h"
+#include "spritegroup_anim.h"
 
-void update_weapon_for_entity(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg);
+
+extern unordered_map<entityid, spritegroup_t*> spritegroups;
+
+static inline void update_weapon_for_entity(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg) {
+    massert(g, "gamestate is NULL");
+    massert(id != ENTITYID_INVALID, "entity id is -1");
+    massert(sg, "spritegroup is NULL");
+
+    spritegroup_t* w_sg = nullptr;
+    entityid weaponid = ENTITYID_INVALID;
+    int ctx = -1;
+
+    weaponid = g->ct.get<equipped_weapon>(id).value_or(ENTITYID_INVALID);
+    if (weaponid == ENTITYID_INVALID)
+        return;
+    w_sg = spritegroups[weaponid];
+    if (!w_sg)
+        return;
+
+    ctx = sg->sprites2->at(sg->current)->currentcontext;
+    spritegroup_setcontexts(w_sg, ctx);
+
+    // this really should be either SLASH_F or SLASH_B
+    // eventually we will select this based on other factors as well
+    // or rather we'll have a better way to select which animation joins the entity's attack
+    // lets test this theory by changing it to the B anim
+
+    //spritegroup_set_current(w_sg, SG_ANIM_LONGSWORD_SLASH_B);
+    spritegroup_set_current(w_sg, SG_ANIM_LONGSWORD_SLASH_F);
+}
