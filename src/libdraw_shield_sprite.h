9ceb9a6b6543fe363b7fcad7bdc0dc4aba557fb7
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-13 20:02:47 -0600

    got rid of a .cpp

--- src/libdraw_shield_sprite.h
+++ src/libdraw_shield_sprite.h
@@ -1,6 +1,24 @@
 
 #include "gamestate.h"
+#include "libdraw_get_shield_sprite.h"
 #include "spritegroup.h"
 
-void draw_shield_sprite_back(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg);
-void draw_shield_sprite_front(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg);
+
+static inline void draw_shield_sprite_back(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg) {
+    massert(g, "gamestate is NULL");
+    massert(id != ENTITYID_INVALID, "id is invalid");
+    massert(sg, "spritegroup is NULL");
+    auto shield_back_s = get_shield_back_sprite(g, id, sg);
+    if (shield_back_s)
+        DrawTexturePro(*shield_back_s->texture, shield_back_s->src, sg->dest, (Vector2){0, 0}, 0, WHITE);
+}
+
+
+static inline void draw_shield_sprite_front(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg) {
+    massert(g, "gamestate is NULL");
+    massert(id != ENTITYID_INVALID, "id is invalid");
+    massert(sg, "spritegroup is NULL");
+    auto shield_front_s = get_shield_front_sprite(g, id, sg);
+    if (shield_front_s)
+        DrawTexturePro(*shield_front_s->texture, shield_front_s->src, sg->dest, (Vector2){0, 0}, 0, WHITE);
+}
