5fd2230dedfba926af03e715822f563658e33f3e
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-13 20:00:17 -0600

    got rid of a .cpp

--- src/libdraw_weapon_sprite.h
+++ src/libdraw_weapon_sprite.h
@@ -1,5 +1,24 @@
 #pragma once
 #include "gamestate.h"
+#include "libdraw_get_weapon_sprite.h"
 #include "spritegroup.h"
-void draw_weapon_sprite_back(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg);
-void draw_weapon_sprite_front(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg);
+
+
+static inline void draw_weapon_sprite_back(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg) {
+    massert(g, "gamestate is NULL");
+    massert(id != ENTITYID_INVALID, "id is invalid");
+    massert(sg, "spritegroup is NULL");
+    auto weapon_back_s = get_weapon_back_sprite(g, id, sg);
+    if (weapon_back_s)
+        DrawTexturePro(*weapon_back_s->texture, weapon_back_s->src, sg->dest, (Vector2){0, 0}, 0, WHITE);
+}
+
+
+static inline void draw_weapon_sprite_front(shared_ptr<gamestate> g, entityid id, spritegroup_t* sg) {
+    massert(g, "gamestate is NULL");
+    massert(id != ENTITYID_INVALID, "id is invalid");
+    massert(sg, "spritegroup is NULL");
+    auto weapon_front_s = get_weapon_front_sprite(g, id, sg);
+    if (weapon_front_s)
+        DrawTexturePro(*weapon_front_s->texture, weapon_front_s->src, sg->dest, (Vector2){0, 0}, 0, WHITE);
+}
