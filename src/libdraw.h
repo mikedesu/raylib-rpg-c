63ac81b0a919b4b08587778a1189f66295f45210
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-14 22:30:19 -0600

    cleanup

--- src/libdraw.h
+++ src/libdraw.h
@@ -10,38 +10,26 @@
 #include "libdraw_from_texture.h"
 #include "libdraw_to_texture.h"
 #include "libdraw_update_sprites.h"
+#include "libgame_defines.h"
 #include "load_music.h"
 #include "load_sfx.h"
 #include "load_textures.h"
+#include "scene.h"
 #include "shaders.h"
 #include "unload_textures.h"
 
-//bool libdraw_windowshouldclose(shared_ptr<gamestate> g);
-//void libdraw_init(shared_ptr<gamestate> g);
-//void libdraw_init_rest(shared_ptr<gamestate> g);
-//void libdraw_drawframe(shared_ptr<gamestate> g);
-//void libdraw_close();
-//void libdraw_close_partial();
-//void libdraw_update_sprites(shared_ptr<gamestate> g);
-
 unordered_map<entityid, spritegroup_t*> spritegroups;
-
 textureinfo txinfo[GAMESTATE_SIZEOFTEXINFOARRAY];
-
 unordered_map<int, Shader> shaders;
-
 RenderTexture2D title_target_texture = {0};
 RenderTexture2D char_creation_target_texture = {0};
 RenderTexture2D main_game_target_texture = {0};
 RenderTexture2D hud_target_texture = {0};
-
 RenderTexture2D target = {0};
 Rectangle target_src = {0, 0, DEFAULT_TARGET_WIDTH, DEFAULT_TARGET_HEIGHT};
 Rectangle target_dest = {0, 0, DEFAULT_TARGET_WIDTH, DEFAULT_TARGET_HEIGHT};
 Rectangle win_dest = {0, 0, DEFAULT_WIN_WIDTH, DEFAULT_WIN_HEIGHT};
-
 Music music;
-
 int ANIM_SPEED = DEFAULT_ANIM_SPEED;
 int libdraw_restart_count = 0;
 
@@ -52,29 +40,27 @@ static inline void libdraw_drawframe(shared_ptr<gamestate> g) {
     BeginDrawing();
     ClearBackground(RED);
     if (g->frame_dirty) {
-        if (g->current_scene == SCENE_TITLE) {
-            draw_title_screen_to_texture(g, false);
-        } else if (g->current_scene == SCENE_MAIN_MENU) {
-            draw_title_screen_to_texture(g, true);
-        } else if (g->current_scene == SCENE_CHARACTER_CREATION) {
-            draw_character_creation_screen_to_texture(g);
-        } else if (g->current_scene == SCENE_GAMEPLAY) {
-            libdraw_drawframe_2d_to_texture(g);
+        switch (g->current_scene) {
+        case SCENE_TITLE: draw_title_screen_to_texture(g, false); break;
+        case SCENE_MAIN_MENU: draw_title_screen_to_texture(g, true); break;
+        case SCENE_CHARACTER_CREATION: draw_character_creation_screen_to_texture(g); break;
+        case SCENE_GAMEPLAY: libdraw_drawframe_2d_to_texture(g); break;
+        default: break;
         }
+
         g->frame_dirty = false;
         g->frame_updates++;
     }
     // draw to the target texture
     BeginTextureMode(target);
     ClearBackground(BLUE);
-    if (g->current_scene == SCENE_TITLE)
-        draw_title_screen_from_texture(g);
-    else if (g->current_scene == SCENE_MAIN_MENU)
-        draw_title_screen_from_texture(g);
-    else if (g->current_scene == SCENE_CHARACTER_CREATION)
-        draw_character_creation_screen_from_texture(g);
-    else if (g->current_scene == SCENE_GAMEPLAY)
-        libdraw_drawframe_2d_from_texture(g);
+    switch (g->current_scene) {
+    case SCENE_TITLE: draw_title_screen_from_texture(g); break;
+    case SCENE_MAIN_MENU: draw_title_screen_from_texture(g); break;
+    case SCENE_CHARACTER_CREATION: draw_character_creation_screen_from_texture(g); break;
+    case SCENE_GAMEPLAY: libdraw_drawframe_2d_from_texture(g); break;
+    default: break;
+    }
     EndTextureMode();
     // draw the target texture to the window
     win_dest.width = GetScreenWidth();
@@ -90,37 +76,29 @@ static inline void libdraw_drawframe(shared_ptr<gamestate> g) {
 static inline void libdraw_init_rest(shared_ptr<gamestate> g) {
     SetExitKey(KEY_ESCAPE);
     SetTargetFPS(60);
-    int w = DEFAULT_WIN_WIDTH;
-    int h = DEFAULT_WIN_HEIGHT;
-    int target_w = DEFAULT_TARGET_WIDTH;
-    int target_h = DEFAULT_TARGET_HEIGHT;
-    massert(w > 0 && h > 0, "window width or height is not set properly");
-    g->windowwidth = w;
-    g->windowheight = h;
-    g->targetwidth = target_w;
-    g->targetheight = target_h;
+    g->windowwidth = DEFAULT_WIN_WIDTH, g->windowheight = DEFAULT_WIN_HEIGHT;
+    g->targetwidth = DEFAULT_TARGET_WIDTH, g->targetheight = DEFAULT_TARGET_HEIGHT;
     TextureFilter filter = TEXTURE_FILTER_POINT; // Use trilinear filtering for better quality
     //TextureFilter filter = TEXTURE_FILTER_BILINEAR; // Use trilinear filtering for better quality
     //TextureFilter filter = TEXTURE_FILTER_TRILINEAR; // Use trilinear filtering for better quality
     //TextureFilter filter = TEXTURE_FILTER_ANISOTROPIC_16X;
-    target = LoadRenderTexture(target_w, target_h);
-    //SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
+    target = LoadRenderTexture(DEFAULT_TARGET_WIDTH, DEFAULT_TARGET_HEIGHT);
     SetTextureFilter(target.texture, filter);
+    //SetTextureFilter(target.texture, TEXTURE_FILTER_BILINEAR);
     // Use anisotropic filtering for better quality
-    title_target_texture = LoadRenderTexture(target_w, target_h);
+    title_target_texture = LoadRenderTexture(DEFAULT_TARGET_WIDTH, DEFAULT_TARGET_HEIGHT);
+    char_creation_target_texture = LoadRenderTexture(DEFAULT_TARGET_WIDTH, DEFAULT_TARGET_HEIGHT);
+    main_game_target_texture = LoadRenderTexture(DEFAULT_TARGET_WIDTH, DEFAULT_TARGET_HEIGHT);
+    hud_target_texture = LoadRenderTexture(DEFAULT_TARGET_WIDTH, DEFAULT_TARGET_HEIGHT);
     SetTextureFilter(title_target_texture.texture, filter);
-    char_creation_target_texture = LoadRenderTexture(target_w, target_h);
     SetTextureFilter(char_creation_target_texture.texture, filter);
-    main_game_target_texture = LoadRenderTexture(target_w, target_h);
     SetTextureFilter(main_game_target_texture.texture, filter);
-    hud_target_texture = LoadRenderTexture(target_w, target_h);
     SetTextureFilter(hud_target_texture.texture, filter);
-    target_src = (Rectangle){0, 0, target_w * 1.0f, -target_h * 1.0f};
-    target_dest = (Rectangle){0, 0, target_w * 1.0f, target_h * 1.0f};
+    target_src = (Rectangle){0, 0, DEFAULT_TARGET_WIDTH * 1.0f, -DEFAULT_TARGET_HEIGHT * 1.0f};
+    target_dest = (Rectangle){0, 0, DEFAULT_TARGET_WIDTH * 1.0f, DEFAULT_TARGET_HEIGHT * 1.0f};
     load_textures(txinfo);
     load_shaders();
-    const float x = target_w / 2.0f;
-    const float y = target_h / 4.0f;
+    const float x = DEFAULT_TARGET_WIDTH / 2.0f, y = DEFAULT_TARGET_HEIGHT / 4.0f;
     g->cam2d.offset = (Vector2){x, y};
     draw_title_screen_to_texture(g, false);
     draw_character_creation_screen_to_texture(g);
