568ccf766f4194a507ceef9aad431fb536092b21
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-15 13:51:28 -0600

    example update towards automation

--- src/tx_keys_boxes.h
+++ src/tx_keys_boxes.h
@@ -3,4 +3,4 @@
 #include "texture_ids.h"
 // wooden box
 #define TX_WOODEN_BOX_COUNT 1
-static int TX_WOODEN_BOX_KEYS[TX_WOODEN_BOX_COUNT] = {TX_WOODEN_BOX};
+static int TX_WOODEN_BOX_KEYS[TX_WOODEN_BOX_COUNT] = {TX_BOX_WOODEN};
