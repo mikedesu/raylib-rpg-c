2a2910fd61220e47e365aee54cf19100fad46481
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-14 20:53:42 -0600

    header-only implementations

--- src/gameloader.h
+++ src/gameloader.h
@@ -1,20 +1,27 @@
 #pragma once
 #include "gamestate.h"
+#include "inputstate.h"
 #include "libdraw.h"
 #include "liblogic.h"
 
 using std::make_shared;
 using std::shared_ptr;
 
+shared_ptr<inputstate> is = make_shared<inputstate>();
+shared_ptr<gamestate> g = gamestateinitptr();
+
+
+#ifndef WEB
 static inline void gamerun() {
     shared_ptr<inputstate> is = make_shared<inputstate>();
     shared_ptr<gamestate> g = gamestateinitptr();
     liblogic_init(g);
     libdraw_init(g);
     while (!libdraw_windowshouldclose(g)) {
-        inputstate_update(is);
-        liblogic_tick(is, g);
-        libdraw_drawframe(g);
+        gameloop();
+        //inputstate_update(is);
+        //liblogic_tick(is, g);
+        //libdraw_drawframe(g);
         //if (g->do_restart) {
         //    minfo("Restarting game...");
         //    libdraw_close_partial();
@@ -31,3 +38,17 @@ static inline void gamerun() {
     liblogic_close(g);
     gamestate_free(g);
 }
+#else
+void gamerun() {
+    shared_ptr<inputstate> is = make_shared<inputstate>();
+    shared_ptr<gamestate> g = gamestateinitptr();
+    // i think liblogic will need init before libdraw because
+    // we will want to create entries in the entitymap AFTER it is init'd
+    liblogic_init(g);
+    libdraw_init(g);
+    emscripten_set_main_loop(gameloop, 0, 1);
+    libdraw_close();
+    liblogic_close(g);
+    gamestate_free(g);
+}
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,53 @@
-#include "gameloader.h"
+//#include "gameloader.h"
+#include "gamestate.h"
+#include "inputstate.h"
+#include "libdraw.h"
+#include "liblogic.h"
+
+#ifdef WEB
+#include <emscripten/emscripten.h>
+#endif
+
+shared_ptr<inputstate> is = make_shared<inputstate>();
+
+shared_ptr<gamestate> g = gamestateinitptr();
+
+
+void gameloop() {
+    inputstate_update(is);
+    liblogic_tick(is, g);
+    libdraw_drawframe(g);
+    //if (g->do_restart) {
+    //    msuccess("Restarting game...");
+    //    libdraw_close();
+    //dlclose(draw_handle);
+    //    liblogic_close(g);
+    //dlclose(logic_handle);
+    //    gamestatefree(g);
+    //    g = gamestateinitptr();
+    //load_draw_symbols();
+    //load_logic_symbols();
+    //    liblogic_init(g);
+    //    libdraw_init(g);
+    //    g->do_restart = false; // Reset restart flag
+    //}
+}
+
 
 int main() {
-    gamerun();
+    liblogic_init(g);
+    libdraw_init(g);
+
+#ifndef WEB
+    while (!libdraw_windowshouldclose(g)) {
+        gameloop();
+    }
+#else
+    emscripten_set_main_loop(gameloop, 0, 1);
+#endif
+    libdraw_close();
+    liblogic_close(g);
+    gamestate_free(g);
+
     return 0;
 }
