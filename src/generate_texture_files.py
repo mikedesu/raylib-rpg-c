691b6d674b8e005b2f5fc0438c21d03fd0422e86
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-15 16:00:13 -0600

    preparing...

--- src/generate_texture_files.py
+++ src/generate_texture_files.py
@@ -1,15 +1,45 @@
 import os
+from PIL import Image
 
 
 def main():
-    for i, j, k in os.walk("img"):
+
+    outfile_images_path = "textures2.txt"
+    outfile_header_path = "textures2.h"
+
+    outfile_images = open(outfile_images_path, "w")
+    outfile_header = open(outfile_header_path, "w")
+
+    filecount = 0
+    for i, _, k in os.walk("img"):
         subpath = i[4:]
         for file in k:
             path = f"{subpath}/{file}"
+            fullpath = f"img/{path}"
             macro = path.replace("/", "_")
             macro = macro[:-4]
             macro = macro.upper()
-            print(f"{macro}")
+            macro = f"TX_{macro}"
+
+            # print(f"#define {macro} {filecount}")
+            outfile_header.write(f"#define {macro} {filecount}\n")
+
+            img = Image.open(fullpath)
+            w = img.size[0]
+            h = img.size[1]
+
+            num_contexts = h // 32
+            frame_count = w // 32
+
+            # print(f"{filecount} {num_contexts} {frame_count} 0 {fullpath}")
+            outfile_images.write(
+                f"{filecount} {num_contexts} {frame_count} 0 {fullpath}\n"
+            )
+
+            filecount += 1
+
+    outfile_images.close()
+    outfile_header.close()
 
 
 if __name__ == "__main__":
