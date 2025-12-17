a2fc620c5a19b178b445c53059c82cb860f37eb4
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-17 14:15:59 -0600

    moved set_sg_is_attacking to set_sg.h

--- src/set_sg.h
+++ src/set_sg.h
@@ -3,6 +3,7 @@
 
 #include "gamestate.h"
 #include "libdraw_update_shield_for_entity.h"
+#include "libdraw_update_weapon_for_entity.h"
 #include "spritegroup.h"
 #include "spritegroup_anim.h"
 
@@ -66,4 +67,24 @@ static inline void libdraw_set_sg_is_dead(shared_ptr<gamestate> g, entityid id, 
     spritegroup_set_current(sg, sg->default_anim);
     //sg_reset_anim(sg);
     spritegroup_set_stop_on_last_frame(sg, true);
+}
+
+
+static inline void libdraw_set_sg_is_attacking(shared_ptr<gamestate> g, entityid id, spritegroup_t* const sg) {
+    massert(g, "gamestate is NULL");
+    massert(id != ENTITYID_INVALID, "entityid is invalid");
+    massert(sg, "spritegroup is NULL");
+    //weapontype wtype = g_get_weapontype(g, weapon);
+    int cur = SG_ANIM_NPC_ATTACK;
+    //if (wtype == WEAPON_BOW) {
+    //    cur = SG_ANIM_NPC_SHOT;
+    //}
+    //if (race == RACE_BAT) {
+    //    cur = SG_ANIM_BAT_ATTACK;
+    //} else if (race == RACE_GREEN_SLIME) {
+    //    cur = SG_ANIM_SLIME_ATTACK;
+    //}
+    spritegroup_set_current(sg, cur);
+    update_weapon_for_entity(g, id, sg);
+    g->ct.set<attacking>(id, false);
 }
