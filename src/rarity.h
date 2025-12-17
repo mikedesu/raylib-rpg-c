106a10fb589daec12d86c79b9dcb5acd8d2f85ad
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-17 02:24:49 -0600

    rarity

---
+++ src/rarity.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+typedef enum
+{
+    RARITY_NONE,
+    RARITY_COMMON,
+    RARITY_UNCOMMON,
+    RARITY_RARE,
+    RARITY_EPIC,
+    RARITY_LEGENDARY,
+    RARITY_COUNT,
+} rarity_t;
