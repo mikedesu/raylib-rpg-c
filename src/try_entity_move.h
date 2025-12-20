0d88ffe64080cdf72f367335d206abcbd64f2215
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-19 19:21:19 -0600

    boxes can push each other

--- src/try_entity_move.h
+++ src/try_entity_move.h
@@ -12,7 +12,6 @@
 #include "tile_npc_living_count.h"
 
 static inline bool try_entity_move(gamestate& g, entityid id, vec3 v) {
-    //massert(g, "Game state is NULL!");
     massert(id != ENTITYID_INVALID, "Entity ID is invalid!");
     minfo("try_entity_move: %d, (%d,%d,%d)", id, v.x, v.y, v.z);
     g.ct.set<update>(id, true);
@@ -26,16 +25,8 @@ static inline bool try_entity_move(gamestate& g, entityid id, vec3 v) {
     // we will have a special case for traversing floors so ignore v.z
     const vec3 aloc = {loc.x + v.x, loc.y + v.y, loc.z};
     auto df = d_get_floor(g.dungeon, loc.z);
-    //if (!df) {
-    //    merror("Dungeon floor %d does not exist", loc.z);
-    //    return false;
-    //}
     // i feel like this might be something we can set elsewhere...like after the player input phase?
     auto tile = df_tile_at(df, aloc);
-    //if (!tile) {
-    //    merror("Tile does not exist at (%d, %d, %d)", aloc.x, aloc.y, aloc.z);
-    //    return false; // Tile does not exist
-    //}
     if (!tile_is_walkable(tile.type)) {
         merror("Tile at (%d, %d, %d) is not walkable", aloc.x, aloc.y, aloc.z);
         return false;
@@ -49,16 +40,14 @@ static inline bool try_entity_move(gamestate& g, entityid id, vec3 v) {
         minfo("Box present");
         const bool maybe_pushable = g.ct.get<pushable>(maybe_box_id).value_or(false);
         if (maybe_pushable) {
-            const vec3 box_loc = g.ct.get<location>(maybe_box_id).value_or((vec3){-1, -1, -1});
-            const vec3 box_new_loc = {box_loc.x + v.x, box_loc.y + v.y, box_loc.z};
-
-            tile_t& box_new_loc_tile = df_tile_at(d_get_floor(g.dungeon, g.dungeon.current_floor), box_new_loc);
-            if (tile_entity_count(box_new_loc_tile) == 0) {
-                try_entity_move(g, maybe_box_id, v);
+            //const vec3 box_loc = g.ct.get<location>(maybe_box_id).value_or((vec3){-1, -1, -1});
+            //const vec3 box_new_loc = {box_loc.x + v.x, box_loc.y + v.y, box_loc.z};
+            //tile_t& box_new_loc_tile = df_tile_at(d_get_floor(g.dungeon, g.dungeon.current_floor), box_new_loc);
+            //if (tile_entity_count(box_new_loc_tile) == 0) {
+            if (try_entity_move(g, maybe_box_id, v)) {
                 return true;
             } else {
-                // not an empty tile
-                minfo("tile not empty");
+                minfo("try entity move failed");
                 return false;
             }
 
@@ -95,9 +84,7 @@ static inline bool try_entity_move(gamestate& g, entityid id, vec3 v) {
 
 
     const entityid maybe_door = tile_has_door(g, aloc);
-    if (maybe_door == ENTITYID_INVALID) {
-        minfo("No door...");
-    } else {
+    if (maybe_door != ENTITYID_INVALID) {
         minfo("Tile has door");
         auto maybe_is_open = g.ct.get<door_open>(maybe_door);
         if (maybe_is_open.has_value()) {
@@ -134,10 +121,8 @@ static inline bool try_entity_move(gamestate& g, entityid id, vec3 v) {
     g.ct.set<location>(id, aloc);
     g.ct.set<spritemove>(id, (Rectangle){mx, my, 0, 0});
 
-    //play_sound_if_heard(SFX_STEP_STONE_1, check_hearing(g, g->hero_id, aloc));
-    if (check_hearing(g, g.hero_id, aloc)) {
+    if (check_hearing(g, g.hero_id, aloc))
         PlaySound(g.sfx[SFX_STEP_STONE_1]);
-    }
 
     return true;
 }
