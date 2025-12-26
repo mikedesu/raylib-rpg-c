8249b8a430c363f7f669f2d98a1afaced81637b3
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-26 00:43:12 -0600

    removed old .h files

--- src/tile_has_live_npcs.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "gamestate.h"
-#include "recompute_entity_cache.h"
-
-static inline bool tile_has_live_npcs(gamestate& g, tile_t& t) {
-    recompute_entity_cache(g, t);
-    return t.cached_live_npcs > 0;
-}
