adf89e2f405924c58d7688a9ebf5857406214c2b
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-27 11:50:13 -0600

    cleanup

--- src/dungeon.h
+++ src/dungeon.h
@@ -42,6 +42,13 @@ public:
 
 
 
+    const inline size_t get_floor_count() {
+        return floors.size();
+    }
+
+
+
+
     inline dungeon_floor_t& get_floor(const size_t index) {
         massert(index >= 0 && index < floors.size(), "index is OOB");
         return floors[index];
@@ -67,19 +74,19 @@ public:
         auto creation_rules = [&df]() {
             const float x = df.width / 4.0;
             const float y = df.height / 4.0;
-            const int w = 4;
-            const int h = 4;
+            constexpr int w = 4;
+            constexpr int h = 4;
             // create an wxh area
-            Rectangle base = {x - 1, y - 1, static_cast<float>(df.width), static_cast<float>(df.height)};
+            const Rectangle base = {x - 1, y - 1, static_cast<float>(df.width), static_cast<float>(df.height)};
             df_set_area(df, TILE_STONE_WALL_01, TILE_STONE_WALL_01, base);
             // draw room 1
-            Rectangle room1 = {base.x + 1, base.y + 1, w, h};
+            const Rectangle room1 = {base.x + 1, base.y + 1, w, h};
             df_set_area(df, TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_09, room1);
             // draw room 2
-            Rectangle room2 = {room1.x + room1.width + 1, room1.y, w, h};
+            const Rectangle room2 = {room1.x + room1.width + 1, room1.y, w, h};
             df_set_area(df, TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_09, room2);
             // poke a hole
-            Rectangle entryway1 = {room2.x - 1, room2.y + 1, 1, 1};
+            const Rectangle entryway1 = {room2.x - 1, room2.y + 1, 1, 1};
             df_set_area(df, TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_09, entryway1);
             // draw room 3
             //Rectangle room3 = {room2.x, room2.y + room2.height + 1, w, h};
@@ -93,7 +100,6 @@ public:
             // poke a hole in the wall
             //Rectangle entryway3 = {room3.x - 1, room3.y + 1, 1, 1};
             //df_set_area(df, TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_09, entryway3);
-            //minfo("setting stairs...");
             // set upstairs
             const vec3 loc_u = {static_cast<int>(room1.x), static_cast<int>(room1.y), df.floor};
             df_set_tile(df, TILE_UPSTAIRS, loc_u.x, loc_u.y);
@@ -102,7 +108,6 @@ public:
             const vec3 loc_d = {static_cast<int>(room1.x + 1), static_cast<int>(room1.y + 1), df.floor};
             df_set_tile(df, TILE_DOWNSTAIRS, loc_d.x, loc_d.y);
             df.downstairs_loc = loc_d;
-            //minfo("setting doors...");
             // automatic door placement
             for (int x = 0; x < df.width; x++) {
                 for (int y = 0; y < df.height; y++) {
@@ -113,23 +118,7 @@ public:
             }
         };
 
-        //minfo("df xform...");
         df_xform(creation_rules);
         floors.push_back(df);
     }
 };
-
-
-
-
-//static inline void d_create(dungeon& dungeon) {
-//minfo("d_create");
-//    if (dungeon.is_initialized) {
-//merror("dungeon is already initialized");
-//        return;
-//    }
-//    dungeon.current_floor = 0;
-//    dungeon.is_locked = false;
-//    dungeon.is_initialized = false;
-//msuccess("dungeon created");
-//}
