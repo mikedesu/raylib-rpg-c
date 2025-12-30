70ad2e3c95d31a94717cf9f8766b8559ac5a1bd7
darkmage <165251+mikedesu@users.noreply.github.com>
2025-12-30 14:34:03 -0600

    prep

--- src/do_unittests.sh
+++ src/do_unittests.sh
@@ -1,6 +1,2 @@
 
-
-
-
-#make clean && CXXFLAGS="-DDEBUG=1 -DDEBUG_ASSERT=1 -DNPCS_ALL_AT_ONCE -DMUSIC_OFF" make -j8 tests && ./tests
-make clean && CXXFLAGS=" -DDEBUG_ASSERT=1 -DNPCS_ALL_AT_ONCE -DMUSIC_OFF" make -j8 tests && ./tests
+make clean && CXXFLAGS=" -DDEBUG=1 -DDEBUG_ASSERT=1 -DNPCS_ALL_AT_ONCE -DMUSIC_OFF" make -j8 tests && ./tests
