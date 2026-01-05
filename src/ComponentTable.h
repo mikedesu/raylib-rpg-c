bb9001cf09f7be36da0b968e16e9202e68ec4483
darkmage <165251+mikedesu@users.noreply.github.com>
2026-01-04 23:06:56 -0600

    cleanup

--- src/ComponentTable.h
+++ src/ComponentTable.h
@@ -19,7 +19,8 @@ using std::type_index;
 using std::unordered_map;
 
 // Component manager
-class ComponentTable {
+class ComponentTable
+{
     // Central storage for all component maps, using shared_ptr<void> to store
     // different shared_ptr<unordered_map<EntityID, T>> types
     unordered_map<type_index, shared_ptr<void>> stores;
@@ -27,10 +28,12 @@ class ComponentTable {
 public:
     // Get or create map for a component kind
     template <typename Kind>
-    shared_ptr<unordered_map<entityid, typename ComponentTraits<Kind>::Type>> getStore() {
+    shared_ptr<unordered_map<entityid, typename ComponentTraits<Kind>::Type>> getStore()
+    {
         auto type = type_index(typeid(Kind));
         auto it = stores.find(type);
-        if (it == stores.end()) {
+        if (it == stores.end())
+        {
             // Create new map for this component kind
             auto store = make_shared<unordered_map<entityid, typename ComponentTraits<Kind>::Type>>();
             stores[type] = store;
@@ -44,31 +47,37 @@ public:
     }
     // Add a component
     template <typename Kind>
-    void set(entityid entity, const typename ComponentTraits<Kind>::Type& component) {
+    void set(entityid entity, const typename ComponentTraits<Kind>::Type& component)
+    {
         (*getStore<Kind>())[entity] = component;
     }
     // Get a component (returns optional)
     template <typename Kind>
-    optional<typename ComponentTraits<Kind>::Type> get(entityid entity) {
+    optional<typename ComponentTraits<Kind>::Type> get(entityid entity)
+    {
         auto store = getStore<Kind>();
         auto it = store->find(entity);
-        if (it == store->end()) {
+        if (it == store->end())
+        {
             return nullopt;
         }
         return it->second;
     }
     // Check if component exists
     template <typename Kind>
-    bool has(entityid entity) {
+    bool has(entityid entity)
+    {
         return getStore<Kind>()->find(entity) != getStore<Kind>()->end();
     }
     // Remove a component (returns if removal succeeded)
     template <typename Kind>
-    bool remove(entityid entity) {
+    bool remove(entityid entity)
+    {
         return getStore<Kind>()->erase(entity) > 0;
     }
     // Empty the table
-    void clear() {
+    void clear()
+    {
         stores.clear();
     }
 };
