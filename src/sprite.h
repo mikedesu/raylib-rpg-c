6c8e963784fce62377ba7e400866ad2a6ac296ba
darkmage <165251+mikedesu@users.noreply.github.com>
2026-01-22 11:46:29 -0600

    cleanup

--- src/sprite.h
+++ src/sprite.h
@@ -41,119 +41,65 @@ public:
         // the width is the current frame times the width of the sprite
         src = Rectangle{
             static_cast<float>(currentframe * width), static_cast<float>(currentcontext * height), static_cast<float>(width), static_cast<float>(height)};
-        //dest = Rectangle{0, 0, 0, 0};
     }
 
     ~sprite() {
     }
 
     void incr_frame() {
-        if (!is_animating)
-            return;
-        currentframe++;
-        if (stop_on_last_frame && currentframe == numframes - 1) {
-            is_animating = false;
+        if (is_animating) {
+            currentframe++;
+            if (stop_on_last_frame && currentframe == numframes-1) {
+                is_animating = false;
+                src.x = width * currentframe;
+            } else if (currentframe >= numframes) {
+                currentframe = 0;
+                num_loops++;
+            }
             src.x = width * currentframe;
-            return;
         }
-        if (currentframe >= numframes) {
-            currentframe = 0;
-            num_loops++;
-        }
-        src.x = width * currentframe;
+        //if (!is_animating)
+        //    return;
+        //currentframe++;
+        //if (stop_on_last_frame && currentframe == numframes - 1) {
+        //    is_animating = false;
+        //    src.x = width * currentframe;
+        //    return;
+        //}
+        //if (currentframe >= numframes) {
+        //    currentframe = 0;
+        //    num_loops++;
+        //}
+        //src.x = width * currentframe;
     }
 
-    int get_width() {
-        return width;
-    }
-
-    int get_height() {
-        return height;
-    }
-
-    void set_width(int w) {
-        width = w;
-    }
-
-    void set_height(int h) {
-        height = h;
-    }
-
-    int get_numcontexts() {
-        return numcontexts;
-    }
-
-    int get_numframes() {
-        return numframes;
-    }
-
-    int get_currentframe() {
-        return currentcontext;
-    }
-
-    int get_currentcontext() {
-        return currentframe;
-    }
-
-    int get_num_loops() {
-        return num_loops;
-    }
-
-    void set_currentcontext(int n) {
-        currentcontext = n;
-    }
-
-    void set_currentframe(int n) {
-        currentframe = n;
-    }
-
-    void set_num_loops(int n) {
-        num_loops = n;
-    }
-
-    Texture2D* get_texture() {
-        return texture;
-    }
-
-    Rectangle get_src() {
-        return src;
-    }
-
-    void set_src(Rectangle r) {
-        src = r;
-    }
-
-    void set_src_x(float x) {
-        src.x = x;
-    }
-
-    void set_src_y(float y) {
-        src.y = y;
-    }
-
-    bool get_is_animating() {
-        return is_animating;
-    }
-
-    void set_is_animating(bool b) {
-        is_animating = b;
-    }
-
-    bool get_stop_on_last_frame() {
-        return stop_on_last_frame;
-    }
-
-    void set_stop_on_last_frame(bool b) {
-        stop_on_last_frame = b;
-    }
+    int get_width() {return width;}
+    int get_height() {return height;}
+    void set_width(int w) {width = w;}
+    void set_height(int h) {height = h;}
+    int get_numcontexts() {return numcontexts;}
+    int get_numframes() {return numframes;}
+    int get_currentframe() {return currentcontext;}
+    int get_currentcontext() {return currentframe;}
+    int get_num_loops() {return num_loops;}
+    void set_currentcontext(int n) {currentcontext = n;}
+    void set_currentframe(int n) {currentframe = n;}
+    void set_num_loops(int n) {num_loops = n;}
+    Texture2D* get_texture() {return texture; }
+    Rectangle get_src() {return src;}
+    void set_src(Rectangle r) {src = r;}
+    void set_src_x(float x) {src.x = x; }
+    void set_src_y(float y) {src.y = y;}
+    bool get_is_animating() {return is_animating;}
+    void set_is_animating(bool b) {is_animating = b;}
+    bool get_stop_on_last_frame() {return stop_on_last_frame;  }
+    void set_stop_on_last_frame(bool b) {stop_on_last_frame = b;}
 
     void set_context(int ctx) {
-        //const int nc = numcontexts;
         massert(ctx >= 0, "set_context: ctx is less than 0: %d", ctx);
         massert(ctx < numcontexts, "set_context: ctx is greater than numcontexts: %d < %d", ctx, numcontexts);
         currentcontext = ctx % numcontexts;
         src.y = height * currentcontext;
-        currentframe = 0;
-        src.x = 0;
+        currentframe = src.x = 0;
     }
 };
