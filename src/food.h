705a0a70073373f5f744b3adfae36795326bfa9e
darkmage <165251+mikedesu@users.noreply.github.com>
2026-01-23 18:19:21 -0600

    almost everything we need to add food and draw it...

--- src/food.h
+++ src/food.h
@@ -1,3 +1,7 @@
 #pragma once
 
-typedef enum { FOOD_NONE, FOOD_COUNT } food_t;
+typedef enum { FOOD_NONE, FOOD_BURGER, FOOD_COFFEE, FOOD_ONIGIRI, FOOD_COUNT } food_t;
+
+typedef struct {
+    int restore_amount;
+} food_attribute_t;
--- src/libdraw_create_spritegroup.h
+++ src/libdraw_create_spritegroup.h
@@ -1,28 +1,24 @@
 #pragma once
 
-
 #include "dungeon_floor.h"
 #include "gamestate.h"
 #include "libgame_defines.h"
 #include "spritegroup.h"
 #include "textureinfo.h"
 
-
 extern textureinfo txinfo[GAMESTATE_SIZEOFTEXINFOARRAY];
 extern unordered_map<entityid, spritegroup*> spritegroups;
-
 
 static inline bool create_spritegroup(gamestate& g, entityid id, int* keys, int num_keys, int offset_x, int offset_y) {
     minfo("BEGIN create_spritegroup");
     massert(txinfo, "txinfo is null");
     // can hold up to 32 sprites
-    //spritegroup* group = spritegroup_create(SPRITEGROUP_DEFAULT_SIZE);
     spritegroup* group = new spritegroup(SPRITEGROUP_DEFAULT_SIZE);
-    
     massert(group, "spritegroup is NULL");
     //disabling this check until dungeon_floor created
     auto df = g.d.get_current_floor();
     auto maybe_loc = g.ct.get<location>(id);
+
     // if it has a location...
     if (maybe_loc.has_value()) {
         const vec3 loc = maybe_loc.value();
@@ -35,7 +31,7 @@ static inline bool create_spritegroup(gamestate& g, entityid id, int* keys, int 
             group->add(s);
         }
         group->id = id;
-        auto s = group->get( 0);
+        auto s = group->get(0);
         massert(s, "sprite is NULL");
         group->current = 0;
         float x = loc.x * DEFAULT_TILE_SIZE + offset_x;
@@ -62,10 +58,10 @@ static inline bool create_spritegroup(gamestate& g, entityid id, int* keys, int 
     //auto s = spritegroup_get(group, 0);
     auto s = group->get(0);
     massert(s, "sprite is NULL");
-    float x = -DEFAULT_TILE_SIZE + offset_x;
-    float y = -DEFAULT_TILE_SIZE + offset_y;
-    float w = s->get_width();
-    float h = s->get_height();
+    const float x = -DEFAULT_TILE_SIZE + offset_x;
+    const float y = -DEFAULT_TILE_SIZE + offset_y;
+    const float w = s->get_width();
+    const float h = s->get_height();
     group->dest = Rectangle{x, y, w, h};
     group->off_x = offset_x;
     group->off_y = offset_y;
