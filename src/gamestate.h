bfce238d535b7715bc596ffa8a2c5d97b2f27f78
darkmage <165251+mikedesu@users.noreply.github.com>
2026-02-02 19:49:22 -0600

    new functions to isolate 2 df layouts

--- src/gamestate.h
+++ src/gamestate.h
@@ -323,8 +323,105 @@ public:
         return d.get_current_floor()->tile_at(loc);
     }
 
-    inline void init_dungeon(biome_t type, int df_count, int w, int h, float parts) {
+
+
+
+    inline void create_and_add_df_0(biome_t type, int df_count, int w, int h, float parts) {
+        auto df = d.create_floor(type, w, h);
+        const float dw = df->get_width();
+        const float dh = df->get_height();
+        vector<room> rooms;
+
+        room r(0, TextFormat("room-%d", 0), TextFormat("room-%d description", 0), Rectangle{0, 0, dw, dh});;
+        df->set_area(TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_11, r.get_area());
+        d.add_floor(df);
+    }
+
+
+
+
+    inline void create_and_add_df_1(biome_t type, int df_count, int w, int h, float parts) {
         constexpr float min_room_w = 2;
+        auto df = d.create_floor(type, w, h);
+        const float dw = df->get_width();
+        const float dh = df->get_height();
+        vector<room> rooms;
+        vector<room> doorways;
+        minfo2("creating rooms");
+        for (int i = 0; i < parts; i++) {
+            for (int j = 0; j < parts; j++) {
+                Rectangle q = {(dw / parts) * j, (dh / parts) * i, dw / parts, dh / parts};
+                const float max_room_w = q.width / 2.0;
+                uniform_int_distribution<int> pgx(q.x, q.x + q.width / 2.0 - 1);
+                uniform_int_distribution<int> pgy(q.y, q.y + q.height / 2.0 - 1);
+                uniform_int_distribution<int> sg(min_room_w, max_room_w);
+                Rectangle ra1 = {static_cast<float>(pgx(mt)), static_cast<float>(pgy(mt)), static_cast<float>(sg(mt)), static_cast<float>(sg(mt))};
+                room_id rid = i * parts + j;
+                room r(rid, TextFormat("room-%d", rid), TextFormat("room-%d description", rid), ra1);
+                rooms.push_back(r);
+            }
+        }
+        minfo2("setting rooms");
+        for (size_t i = 0; i < rooms.size(); i++) {
+            if (!df->add_room(rooms[i])) {
+                merror("Failed to add room");
+                continue;
+            }
+            df->set_area(TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_11, rooms[i].get_area());
+        }
+        minfo2("sorting rooms");
+        std::sort(rooms.begin(), rooms.end(), [](room& a, room& b) { return a.get_x() < b.get_x() && a.get_y() < b.get_y(); });
+        for (size_t y = 0; y < parts; y++) {
+            for (size_t x = 0; x < parts; x++) {
+                const size_t index = y * parts + x;
+                room& r = rooms[index];
+                const int rx = r.get_x() + r.get_w() / 2;
+                const int ry = r.get_y() + r.get_h() / 2;
+                const float rix = rx;
+                const float riy = ry;
+                if (x < parts - 1) {
+                    room& r2 = rooms[index + 1];
+                    const int rx_e = r.get_x() + r.get_w();
+                    const float rxf_e = r.get_x() + r.get_w();
+                    const int rjx_e = r2.get_x();
+                    const float rw = rjx_e - rx_e;
+                    Rectangle c0 = {rxf_e, riy, rw, 1};
+                    df->set_area(TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_11, c0);
+                    shared_ptr<tile_t> first_tile = df->tile_at(vec3{rx_e, ry, -1});
+                    first_tile->set_can_have_door(true);
+                    const int rw_i = rw;
+                    shared_ptr<tile_t> last_tile = df->tile_at(vec3{rx_e + rw_i - 1, ry, -1});
+                    last_tile->set_can_have_door(true);
+                }
+                if (y < parts - 1) {
+                    const size_t index2 = (y + 1) * parts + x;
+                    room& r2 = rooms[index2];
+                    const int ry_e = r.get_y() + r.get_h();
+                    const float ryf_e = r.get_y() + r.get_h();
+                    const int rjy_e = r2.get_y();
+                    const float rh = rjy_e - ry_e;
+                    Rectangle c0 = {rix, ryf_e, 1, rh};
+                    df->set_area(TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_11, c0);
+                    shared_ptr<tile_t> first_tile = df->tile_at(vec3{rx, ry_e, -1});
+                    first_tile->set_can_have_door(true);
+                }
+            }
+        }
+        for (int y = 1; y < dh - 1; y++) {
+            for (int x = 1; x < dw - 1; x++) {
+                const vec3 loc = {x, y, -1};
+                if (df->tile_is_good_for_upgrade(loc))
+                    df->set_area(TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_10, Rectangle{static_cast<float>(loc.x), static_cast<float>(loc.y), 1, 1});
+            }
+        }
+
+        d.add_floor(df);
+    }
+
+
+
+    inline void init_dungeon(biome_t type, int df_count, int w, int h, float parts) {
+        //constexpr float min_room_w = 2;
         minfo2("init_dungeon");
         massert(df_count > 0, "df_count is <= 0");
         massert(w > 0, "w == 0");
@@ -337,20 +434,16 @@ public:
             return;
         }
         for (int i = 0; i < df_count; i++) {
+          
+            create_and_add_df_0(type, df_count, w, h, parts);
+
+
+            /*
             auto df = d.create_floor(type, w, h);
             const float dw = df->get_width();
             const float dh = df->get_height();
-            // center of df
             vector<room> rooms;
             vector<room> doorways;
-            // no matter the size of the quad, they will conform to the dungeon size
-            // but keep things divisible by 2 to make the algorithm happy :)
-            // rooms of size 1 should be non-existent
-            // so the minimum room size should be 2x2
-            //constexpr float min_room_h = 2;
-            // we can calculate how many rooms we can accomodate
-            // it can be between 1 big room and quad.width / (min_room_w+1)
-            // but do we need to?
             minfo2("creating rooms");
             for (int i = 0; i < parts; i++) {
                 for (int j = 0; j < parts; j++) {
@@ -365,13 +458,6 @@ public:
                     rooms.push_back(r);
                 }
             }
-            // We've generated some rooms, nice
-            // because the rooms can be quite distant from each other,
-            // we need to walk through and create some 'connecting' rooms
-            // the catch is that, because we have not yet created the rooms, we've only added them
-            // to our rooms vector, we can't yet test the tiles themselves...
-            // we could create the rooms right away. this might make checking things easier because
-            // we want to make sure we leave a gap between rooms
             minfo2("setting rooms");
             for (size_t i = 0; i < rooms.size(); i++) {
                 if (!df->add_room(rooms[i])) {
@@ -380,34 +466,21 @@ public:
                 }
                 df->set_area(TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_11, rooms[i].get_area());
             }
-            // now that the rooms have been laid down onto tiles, we can step through
-            // and find placed we can fit some additional small (or large) rooms
-            // in that last dungeon, there was a good space for a connecting room
-            // in THAT last dungeon, there was not good space for any connecting rooms
-            // in situations like that, we might just wire the rooms up directly
-            // note that the rooms are not sorted
-            // we might want to do that to make making the hallways easier
             minfo2("sorting rooms");
             std::sort(rooms.begin(), rooms.end(), [](room& a, room& b) { return a.get_x() < b.get_x() && a.get_y() < b.get_y(); });
-            // hallways
             for (size_t y = 0; y < parts; y++) {
                 for (size_t x = 0; x < parts; x++) {
                     const size_t index = y * parts + x;
                     room& r = rooms[index];
-                    // calc center of room
                     const int rx = r.get_x() + r.get_w() / 2;
                     const int ry = r.get_y() + r.get_h() / 2;
                     const float rix = rx;
                     const float riy = ry;
                     if (x < parts - 1) {
                         room& r2 = rooms[index + 1];
-                        // calc edge of room 1
                         const int rx_e = r.get_x() + r.get_w();
                         const float rxf_e = r.get_x() + r.get_w();
-                        // calc center x of room 2
-                        // calc edge of room 2
                         const int rjx_e = r2.get_x();
-                        // calc width of hallway
                         const float rw = rjx_e - rx_e;
                         Rectangle c0 = {rxf_e, riy, rw, 1};
                         df->set_area(TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_11, c0);
@@ -431,11 +504,6 @@ public:
                     }
                 }
             }
-            // Now we want to introduce a one-pass 'stitching' algorithm
-            // The hallways may be unconnected at their ends
-            // If we iterate each tile across the floor
-            // and check to see if it meets certain conditions
-            // we can place a new tile to help link rooms and halls together
             for (int y = 1; y < dh - 1; y++) {
                 for (int x = 1; x < dw - 1; x++) {
                     const vec3 loc = {x, y, -1};
@@ -443,8 +511,13 @@ public:
                         df->set_area(TILE_FLOOR_STONE_00, TILE_FLOOR_STONE_10, Rectangle{static_cast<float>(loc.x), static_cast<float>(loc.y), 1, 1});
                 }
             }
+
+
+
+
             minfo2("adding floor to dungeon...");
             d.add_floor(df);
+            */
         }
         minfo2("END floor creation loop");
         d.is_initialized = true;
