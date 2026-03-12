b589d76a5b9a3d1f82f272b120a387053200eb42
darkmage <165251+mikedesu@users.noreply.github.com>
2026-03-11 22:07:18 -0500

    version

--- src/libgame_defines.h
+++ src/libgame_defines.h
@@ -2,7 +2,7 @@
 
 #ifndef GAME_INFO_DEFINES
 #define GAME_INFO_DEFINES
-#define GAME_VERSION "v0.0.6.19"
+#define GAME_VERSION "v0.0.6.20"
 #define GAME_VERSION_DATE "March 11, 2026"
 #define WINDOW_TITLE "@evildojo666 presents: project.rpg"
 #endif
